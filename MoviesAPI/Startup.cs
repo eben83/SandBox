2ca8f32aed650fc7ddb068a5fd05da0cd6838f6a
Eben Burger <eben_burger@me.com>
2019-12-12 16:48:09 +0200

    some testing changes

--- MoviesAPI/Startup.cs
+++ MoviesAPI/Startup.cs
@@ -5,42 +5,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
+using MoviesAPI.Services;
 namespace MoviesAPI
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            var connectionString =
+     public class Startup
+     {
+          public void ConfigureServices(IServiceCollection services
+          {
+               var connectionString =
 "Server=localhost;Database=MoviesDB;User Id=sa;Password=Passw0rd!";
                services
                  .AddDbContext<MoviesDbContext>(o =>
                    o.UseSqlServer(connectionString));
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MoviesDbContext moviesDbContext)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseStaticFiles();
-
-            moviesDbContext.CreateSeedData();
-
-            app.Run(async (context) =>
+          }
+          public void Configure(IApplicationBuilder app,
+               IHostingEnvironment env,
+               MoviesDbContext moviesDbContext)
+          {
+               if (env.IsDevelopment())
+               {
+                    app.UseDeveloperExceptionPage();
+               }
+               app.UseStaticFiles();
+               moviesDbContext.CreateSeedData();
+               app.Run(async (context) =>
                {
                     await context
                            .Response
                            .WriteAsync("Hello World!");
                });
-        }
-    }
+          }
+     }
 }
