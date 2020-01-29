e56627eb2b34254c99a1539bcd9064176e01516b
Eben Burger <eben_burger@me.com>
2020-01-29 09:10:32 +0200

    Bl Coffee Project

---
+++ BLcoffeeMachine/BLcoffeeMachine/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BLcoffeeMachine.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
