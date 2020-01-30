3685f028c1842357d137119622d95b7c9dfb2959
Eben Burger <eben_burger@me.com>
2020-01-30 06:47:57 +0200

    Bl Coffee Machine changes

---
+++ gradbook/src/GradBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GradBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
