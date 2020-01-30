d3bde777059d102534e3f3ff97eda60eced88b34
Eben Burger <eben_burger@me.com>
2020-01-30 10:31:01 +0200

    files added to C#

--- gradbook/src/GradBook/Program.cs
+++ gradbook/src/GradBook/Program.cs
@@ -4,9 +4,9 @@ namespace GradBook
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //main is a method- in the () is the paramertes, arg is the name
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Eben");
         }
     }
 }
