5b7dec098bfecf2c435e2d0ee684262dec12efb7
Eben Burger <eben_burger@me.com>
2020-01-30 12:05:55 +0200

    playing with method and debug

--- gradbook/src/GradBook/Program.cs
+++ gradbook/src/GradBook/Program.cs
@@ -6,7 +6,16 @@ namespace GradBook
     {
         static void Main(string[] args) //main is a method- in the () is the paramertes, arg is the name
         {
-            Console.WriteLine("Hello Eben");
+            if (args.Length > 0) 
+            {
+
+            Console.WriteLine("Hello " + args[0] + "!"); //concat strings, adding the args sub zero from the array
+
+            Console.WriteLine($"Hello, {args[0]} !"); //string interpelation
+            }
+            else {
+                Console.WriteLine("Hello");
+            }
         }
     }
 }
