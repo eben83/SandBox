87136ab7aca4d31b36e954da3b8e5818c86fc52e
Eben Burger <eben_burger@me.com>
2020-01-31 14:57:26 +0200

    methods

--- gradbook/src/GradBook/Book.cs
+++ gradbook/src/GradBook/Book.cs
@@ -14,7 +14,19 @@ namespace GradBook
 
         public void AddGrade(double grade) // public instnce of the method
         {
-            grades.Add(grade);
+
+            if(grade == 90.90) // this is the equal too
+            {
+                
+            }
+            if(grade <= 100 && grade >= 0) // boolian if statement
+            {
+                grades.Add(grade);
+            }
+            else 
+            {
+                System.Console.WriteLine("Invalid Value");
+            }
         }
         public stats GetStats()
         {
@@ -23,18 +35,56 @@ namespace GradBook
             result.High = double.MinValue;
             result.low = double.MaxValue;
 
+            //jumping statements
+
+            //for loop
+            for(var index2 = 0; index2 < grades.Count; index += 1)
+            {
+                result.low = Math.Min(grades[index2], result.low);
+                result.High = Math.Max(grades[index2], result.High);
+                result.Average += grades[index2];                
+            }
+
+            result.Average /= grades.Count;
+            return result;
+
+            // While Loop
+            // while(index < grades.Count) --this is a while loop the loop will only
+            //                             -- run if the while statement is true.
+            // {
+            //     result.low = Math.Min(grades[index], result.low);
+            //     result.High = Math.Max(grades[index], result.High);
+            //     result.Average += grades[index];
+            //     index += 1;
+                
+            // } ;
+            //result.Average /= grades.Count;
+
+            //do while loop-- will run at least once
+            var index = 0;
+            do 
+            {
+                result.low = Math.Min(grades[index], result.low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average += grades[index];
+                index += 1;
+                
+            } while(index < grades.Count);
+            
+            result.Average /= grades.Count;
+
 
             //for Each statment- 
 
-            foreach(var grade in grades) 
-            {
-                result.low = Math.Min(grade, result.low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
+            // foreach(var grade in grades) 
+            // {
+            //     result.low = Math.Min(grade, result.low);
+            //     result.High = Math.Max(grade, result.High);
+            //     result.Average += grade;
                 
-            }
+            // }
             
-            result.Average /= grades.Count;
+            // result.Average /= grades.Count;
 
             return result;
         }
