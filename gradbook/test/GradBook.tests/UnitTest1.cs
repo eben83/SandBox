0f192a4a6ad2bd06a3989c39d582e285fba16c5b
Eben Burger <eben_burger@me.com>
2020-01-31 10:10:27 +0200

    added test file

--- gradbook/test/GradBook.tests/UnitTest1.cs
+++ gradbook/test/GradBook.tests/UnitTest1.cs
@@ -1,14 +1,13 @@
 using System;
-using Xunit;
+using Xunit; //testing framework
 
-namespace GradBook.test
+namespace GradBook.tests
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-
         }
     }
 }
