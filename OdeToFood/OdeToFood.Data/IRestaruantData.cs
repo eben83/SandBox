48e4127867c4929140f34b64c8beff798e71f141
Eben Burger <eben_burger@me.com>
2020-02-04 20:46:44 +0200

    asp.net update course

---
+++ OdeToFood/OdeToFood.Data/IRestaruantData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaruantData
+    {
+        IEnumerable<Restarant> GetALL();
+
+    }
+
+
+    public class InMemoryRestarantData : IRestaruantData
+    {
+
+        List<Restarant> restarant;
+
+        public InMemoryRestarantData()
+        {
+            restarant = new List<Restarant>()
+            {
+                new Restarant {ID = 1, Name = "Eben's Burger", Location = "JoBurg", Cuisine = CuisineType.Mexican},
+                new Restarant {ID = 2, Name = "Wimpie", Location = "All Over", Cuisine = CuisineType.None},
+                new Restarant {ID = 3, Name = "Star Place", Location = "Pretoria", Cuisine = CuisineType.Italian},
+                new Restarant{ID = 4, Name = "Pots", Location = "Cape Town", Cuisine = CuisineType.Indian},
+
+            };
+        }
+
+        public IEnumerable<Restarant> GetALL()
+        {
+        }
+    }
+}
