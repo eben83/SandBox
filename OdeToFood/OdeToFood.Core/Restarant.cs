09ce8b5191f966bcc7b284f845daf0c56147892a
Eben Burger <eben_burger@me.com>
2020-02-04 14:25:47 +0200

    ASP.NET Core PluralSight

---
+++ OdeToFood/OdeToFood.Core/Restarant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace OdeToFood.Core
+{
+    public class Restarant
+    {
+        /*If a new type is made, "Alt + ." will move it to its own
+         .cs file and keep it all clean...*/
+
+        /* this is the restarant type */
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
+
---
+++ OdeToFood/OdeToFood/Pages/Restarants/List.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood.Pages.Restarants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config; 
+
+        public string Message { get; set; }
+
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        public void OnGet()
+        {
+            Message = config["Message"]; /* this line accesses the AppSetting file*/
+        }
+    }
+}
