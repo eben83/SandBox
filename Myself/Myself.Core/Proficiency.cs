74e781fe171f8725a5c134a79fb7f53d66a07b4c
Eben Burger <eben_burger@me.com>
2020-03-10 13:33:56 +0200

    proficiency method added

---
+++ Myself/Myself.Core/Proficiency.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Myself.Core
+{
+    public class Proficiency
+    {
+        public int ID { get; set; }
+        public string Skilll { get; set; }
+        public ProficiencyType SkillProficiency { get; set; }
+    }
+}
