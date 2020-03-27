e84d9028b1d9dd58466cb32bf123a72e614b31da
Eben Burger <eben_burger@me.com>
2020-03-27 14:09:21 +0200

    js file updated

--- Strava API/strava_api.js
+++ Strava API/strava_api.js
@@ -1,20 +1,37 @@
 
-https://www.strava.com/oauth/token?client_id=45021&client_secret=c258cacbe62c5e597322479531b3211e5cacc844&code=d7b1f2bf90b59a1bb636978fb8147c6b6fe425b6&grant_type=authorization_code
 
-const activities_link = 
-        "https://www.strava.com/api/v3/athlete/activities?access_token=ec2ef468b303dce39d6a1fd31ac02c6b3831442b"
 
-const authorizeation_link = "https://www.strava.com/oauth/token"
+const authorization_link = "https://www.strava.com/oauth/token"
 
-function GetActivities(){
-   
-        fetch(activities_link)
-            .then((res) => console.log(res.json()))
+function GetActivities(res){
+
+    Console.log(res)
+
+    // const activities_link = 
+    //     "https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}"
+
+    //     fetch(activities_link)
+    //         .then((res) => console.log(res.json()))
 }
 
-GetActivities()
 
 function reAuthorize(){
 
+    fetch(authorization_link,{
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
 
+            client_id: '45021',
+            client_secret: 'c258cacbe62c5e597322479531b3211e5cacc844',
+            grant_type: 'refresh_token',
+            refresh_token: 'f28d1c1bdb4ca4b18b9df128891f4d16fd23e2c2'
+        }) 
+    }) .then(res => res.json())
+        .then(res => GetActivities(res))
 }
+
+reAuthorize()
