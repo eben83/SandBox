78440b601cb1ebd0207f8ba70e8ed3ebf69d59bd
Eben Burger <eben_burger@me.com>
2020-03-30 17:34:29 +0200

    Strava services fields added

--- StravaApi/Integration.Strava/StravaService.cs
+++ StravaApi/Integration.Strava/StravaService.cs
@@ -6,8 +6,15 @@ using RestSharp.Authenticators;
 
 namespace Integration.Strava
 {
+    
+
     public class StravaService : IStravaService
     {
+        //fields
+        private const string BASE_URL = "http://www.strava.com";
+        private const string CLIENT_APP = "";
+        private const string CLIENT_SECRET = "";
+        
         public List<ActivityDto> GetActivities()
         {
             var client = new RestClient("");
@@ -15,19 +22,22 @@ namespace Integration.Strava
             
             var request = new RestRequest("");
             request.AddHeader("contentType", "application/json");
-
+        
             var response = client.Get<List<ActivityDto>>(request);
-
+        
             if (!response.IsSuccessful)
                 throw new Exception(response.ErrorMessage);
             
             return response.Data;
-
+        
         }
 
-        public Method Authentication(AuthenticationDetails)
+        public void Authorize()
         {
-           
+            var client = new RestClient(BASE_URL);
+            client.Authenticator = new HttpBasicAuthenticator(CLIENT_APP, CLIENT_SECRET);
+            
+            var request = new RestRequest();
         }
         
     }
