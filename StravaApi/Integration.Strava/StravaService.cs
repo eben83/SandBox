1e5c380332d3c7d477ed653bcd2f6929f96cfa66
Eben Burger <eben_burger@me.com>
2020-03-30 17:54:34 +0200

    strava service fields updated

--- StravaApi/Integration.Strava/StravaService.cs
+++ StravaApi/Integration.Strava/StravaService.cs
@@ -12,8 +12,9 @@ namespace Integration.Strava
     {
         //fields
         private const string BASE_URL = "http://www.strava.com";
-        private const string CLIENT_APP = "";
-        private const string CLIENT_SECRET = "";
+        private const string CLIENT_ID = "45021";
+        private const string CLIENT_ACCESS = "303fb95360e42bd0485d3b89290474b345f0e37b";
+        private const string CLIENT_SECRET = "c258cacbe62c5e597322479531b3211e5cacc844";
         
         public List<ActivityDto> GetActivities()
         {
@@ -35,10 +36,14 @@ namespace Integration.Strava
         public void Authorize()
         {
             var client = new RestClient(BASE_URL);
-            client.Authenticator = new HttpBasicAuthenticator(CLIENT_APP, CLIENT_SECRET);
+            client.Authenticator = new HttpBasicAuthenticator(CLIENT_ID, CLIENT_SECRET);
             
-            var request = new RestRequest();
+            var request = new RestRequest("oauth/authorize");
         }
+
+        public string GetToken()
+        {
+        } 
         
     }
 }
