90dc4875c2cf8c6e8d0e0f858ee7c5f27ca06d1a
Eben Burger <eben_burger@me.com>
2020-03-30 15:42:29 +0200

    Authentication details updated

---
+++ StravaApi/Integration.Strava/Dtos/AuthenticationDetails.cs
@@ -0,0 +1,13 @@
+namespace Integration.Strava.Dtos
+{
+    public class AuthenticationDetails
+    {
+        public void GetAuthenticationDetails()
+        {
+            var TokenType = "token_type";
+            var RefreshToken = "refresh_token";
+            var AccessToken = "access_token";
+            
+        }
+    }
+}
