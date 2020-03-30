e505bee49afd3b37e2f65731cced229cd0f6c7f5
Eben Burger <eben_burger@me.com>
2020-03-30 14:17:29 +0200

    test file added

---
+++ StravaApi/Integration.Strava.Tests/TestIntegration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Integration.Strava.Tests
+{
+    [TestFixture]
+    public class TestIntegration
+    {
+        //[Ignore("Run manually")]
+        [Test]
+        public void GetActivities()
+        {
+            var service = new StravaService();
+
+            var activities = service.GetActivities();
+            
+            Assert.NotNull(activities);
+            Assert.That(activities.Any());
+        }
+    }
+}
