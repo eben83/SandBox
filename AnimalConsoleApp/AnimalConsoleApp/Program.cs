d4f0c84bc0dc8e4cb6187d2efad39ddf2ea8f9a6
Eben Burger <eben_burger@me.com>
2020-03-31 09:09:24 +0200

    Animal Console App added

---
+++ AnimalConsoleApp/AnimalConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace AnimalConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
