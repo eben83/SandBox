06ecef59a9bc04f681561ce607724c594fef80f5
Eben Burger <eben_burger@me.com>
2020-04-01 17:23:01 +0200

    animal methods added

---
+++ AnimalConsoleApp/AnimalConsoleApp/Animals.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace AnimalConsoleApp
+{
+    public class Animals: IAnimal
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public void Run()
+        {
+            Console.WriteLine("Elephant is running....");
+            Console.WriteLine("doof, doof, doof");
+            Console.WriteLine("Look at him go!");
+            Console.WriteLine("Feeling the earth moving!!");
+            Console.WriteLine("Elephant stops Running. He looks quite Hungry");
+            Console.WriteLine("and tired now.");
+            Console.WriteLine();
+            Console.WriteLine("Press enter to carry on.");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Dog is eating...");
+            Console.WriteLine("munch, munch, munch");
+            Console.WriteLine("Look at all that food go down!!!");
+            Console.WriteLine("Burp!!");
+            Console.WriteLine();
+            Console.WriteLine("press enter to carry on.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Cat is sleeping...");
+            Console.WriteLine("ZZzzZZZzzzZZz");
+            Console.WriteLine("how loud can he snore!!!");
+            Console.WriteLine("Cats acn sleep all day");
+        }
+    }
+}
