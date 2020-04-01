eec8838faa61c799c99b25ce6b509e3e83aa2504
Eben Burger <eben_burger@me.com>
2020-04-01 16:29:58 +0200

    renamed interface property

--- AnimalConsoleApp/AnimalConsoleApp/IAnimal.cs
+++ AnimalConsoleApp/AnimalConsoleApp/IAnimal.cs
@@ -3,7 +3,7 @@ namespace AnimalConsoleApp
     public class IAnimal
     {
         string animalType { get; set; }
-        string name { get; set; }
+        string animalName { get; set; }
         string actionRun { get; set; }
         string actionEat { get; set; }
         string actionSleep { get; set; }
