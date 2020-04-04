912a21e54c66d4b0161a7b605cee0291400d01b4
Eben Burger <eben_burger@me.com>
2020-04-04 17:51:40 +0200

    Enums updated

--- AnimalConsoleApp/AnimalConsoleApp/AnimalActions.cs
+++ AnimalConsoleApp/AnimalConsoleApp/AnimalActions.cs
@@ -2,6 +2,8 @@ namespace AnimalConsoleApp
 {
     public enum AnimalActions
     {
-        
+        Run = 1,
+        Sleep = 2,
+        Eat = 3,
     }
 }
--- AnimalConsoleApp/AnimalConsoleApp/AnimalTypes.cs
+++ AnimalConsoleApp/AnimalConsoleApp/AnimalTypes.cs
@@ -2,6 +2,9 @@ namespace AnimalConsoleApp
 {
     public enum AnimalTypes
     {
-        
+        Dog = 1,
+        Cat = 2,
+        Fish = 3,
+        Elephant = 4,
     }
 }
