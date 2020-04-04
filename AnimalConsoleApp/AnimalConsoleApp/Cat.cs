d6483f5770d9032a629dc4684a5050b540064813
Eben Burger <eben_burger@me.com>
2020-04-04 23:11:17 +0200

    updated the override in Cat class

--- AnimalConsoleApp/AnimalConsoleApp/Cat.cs
+++ AnimalConsoleApp/AnimalConsoleApp/Cat.cs
@@ -6,7 +6,10 @@ namespace AnimalConsoleApp
         {
         }
 
-        public AnimalTypes Type { get; set; }
+        public override AnimalTypes Type
+        {
+            get { return AnimalTypes.Cat; }
+        }
         public string Identification { get; set; }
     }
 }
