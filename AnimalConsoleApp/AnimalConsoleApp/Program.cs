8cf8d9691097daa5bb53f4fa0fa7a6b8db51cc68
Eben Burger <eben_burger@me.com>
2020-04-04 15:52:11 +0200

    moved Get And Perform Animal Action function to Ui Engine

--- AnimalConsoleApp/AnimalConsoleApp/Program.cs
+++ AnimalConsoleApp/AnimalConsoleApp/Program.cs
@@ -13,44 +13,5 @@ namespace AnimalConsoleApp
         {
             new UiEngine().Run();
         }
-        
-
-        private static void GetAndPerformAnimalAction(List<Animal> animals)
-        {
-            string animalAction;
-            string animalActionFirstWord;
-            string animalActionSecondWord;
-
-            Console.WriteLine("Animals can do the following:");
-            Console.WriteLine("Run, Sleep, Eat");
-            Console.WriteLine();
-            Console.WriteLine("Specify the animal and action you want to perform:");
-            Console.WriteLine("Example: Elephant - Run / ELEPHANT-RUN / Elephant- Run / Elephant - RUN");
-            animalAction = Console.ReadLine().ToLower().Replace(" ", "");
-            animalActionFirstWord = animalAction.Substring(0, animalAction.IndexOf("-"));
-            animalActionSecondWord = animalAction.Substring(animalAction.IndexOf("-") + 1);
-
-            foreach (var animal in animals)
-            {
-                if (string.Compare(animal.Type, animalActionFirstWord, StringComparison.InvariantCultureIgnoreCase) ==
-                    0)
-                {
-                    if (string.Compare(animalActionSecondWord, "run", StringComparison.InvariantCultureIgnoreCase) == 0)
-                    {
-                        animal.Run();
-                    }
-
-                    if (animalActionSecondWord == "sleep")
-                    {
-                        animal.Sleep();
-                    }
-
-                    if (animalActionSecondWord == "eat")
-                    {
-                        animal.Eat();
-                    }
-                }
-            }
-        }
     }
 }
--- AnimalConsoleApp/AnimalConsoleApp/UiEngine.cs
+++ AnimalConsoleApp/AnimalConsoleApp/UiEngine.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace AnimalConsoleApp
 {
     public class UiEngine
@@ -57,5 +60,43 @@ namespace AnimalConsoleApp
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
         }
+         
+         private void GetAndPerformAnimalAction(List<Animal> animals)
+         {
+             string animalAction;
+             string animalActionFirstWord;
+             string animalActionSecondWord;
+
+             Console.WriteLine("Animals can do the following:");
+             Console.WriteLine("Run, Sleep, Eat");
+             Console.WriteLine();
+             Console.WriteLine("Specify the animal and action you want to perform:");
+             Console.WriteLine("Example: Elephant - Run / ELEPHANT-RUN / Elephant- Run / Elephant - RUN");
+             animalAction = Console.ReadLine().ToLower().Replace(" ", "");
+             animalActionFirstWord = animalAction.Substring(0, animalAction.IndexOf("-"));
+             animalActionSecondWord = animalAction.Substring(animalAction.IndexOf("-") + 1);
+
+             foreach (var animal in animals)
+             {
+                 if (string.Compare(animal.Type, animalActionFirstWord, StringComparison.InvariantCultureIgnoreCase) ==
+                     0)
+                 {
+                     if (string.Compare(animalActionSecondWord, "run", StringComparison.InvariantCultureIgnoreCase) == 0)
+                     {
+                         animal.Run();
+                     }
+
+                     if (animalActionSecondWord == "sleep")
+                     {
+                         animal.Sleep();
+                     }
+
+                     if (animalActionSecondWord == "eat")
+                     {
+                         animal.Eat();
+                     }
+                 }
+             }
+         }
     }
 }
