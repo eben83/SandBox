0454fa1fc60ef35bab26d99154b941906818f701
Eben Burger <eben_burger@me.com>
2020-04-06 18:14:06 +0200

    Get Menu Choice methods added with int.tryParse and if statement

--- AnimalConsoleApp/AnimalConsoleApp/UiEngine.cs
+++ AnimalConsoleApp/AnimalConsoleApp/UiEngine.cs
@@ -1,13 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace AnimalConsoleApp
 {
     public class UiEngine
     {
+        private string userMessage;
+        string menuChoice;
+        
         public void Run()
         {
+           
             
+            while (menuChoice != "4")
+            {
+                Welcome();
+                MainMenu();
+                menuChoice = Console.ReadLine();
+            }
         }
         private void Welcome()
         {
@@ -18,11 +30,34 @@ namespace AnimalConsoleApp
 ​
         private void MainMenu()
         {
+            Console.WriteLine();
             Console.WriteLine("1. Add animal");
             Console.WriteLine("2. Interact with animal");
             Console.WriteLine("3. Interact with all animals");
             Console.WriteLine("4. Exit");
             Console.WriteLine();
         }
+
+        public void GetMenuChoice(string userMessage)
+        {
+            string getChoice;
+            
+            Console.WriteLine(userMessage);
+            bool result = int.TryParse(getChoice = Console.ReadLine(), out int getChoiceInt);
+
+            if (result )
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Sorry your choice is incorrect");
+                Console.WriteLine("Please make another choice.");
+            }
+        }
+            
+            
     }
 }
+
+
