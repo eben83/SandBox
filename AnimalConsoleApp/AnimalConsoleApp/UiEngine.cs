f30ebf4c9de8cef8f3410501e1d50476f799afd8
Eben Burger <eben_burger@me.com>
2020-04-07 19:28:27 +0200

    Get Animal Menu update

--- AnimalConsoleApp/AnimalConsoleApp/UiEngine.cs
+++ AnimalConsoleApp/AnimalConsoleApp/UiEngine.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Globalization;
 using System.Linq;
 
@@ -8,25 +9,27 @@ namespace AnimalConsoleApp
     public class UiEngine
     {
         private const int EXIT_MENU_CHOICE = 4;
-        
+
         public void Run()
         {
             int menuChoice = 0;
-        
+
             while (menuChoice != EXIT_MENU_CHOICE)
             {
                 Welcome();
                 MainMenu();
                 menuChoice = GetMenuChoice("Please make a choice, fool!!!", MainMenu);
-
+                // GetAnimalMenu();
             }
         }
+
         private void Welcome()
         {
             Console.Clear();
             Console.WriteLine("Welcome to my Animal App");
             Console.WriteLine();
         }
+
         private void MainMenu()
         {
             Console.WriteLine();
@@ -37,16 +40,16 @@ namespace AnimalConsoleApp
             Console.WriteLine();
         }
 
-        private int GetMenuChoice( string userMessage, Action menuToShow)
+        private int GetMenuChoice(string userMessage, Action menuToShow)
         {
             var getChoiceInt = 0;
             var isChoiceValid = false;
-            
+
             while (!isChoiceValid)
             {
                 Console.WriteLine(userMessage);
-                
-                if (!int.TryParse(Console.ReadLine(), out getChoiceInt ))
+
+                if (!int.TryParse(Console.ReadLine(), out getChoiceInt))
                 {
                     Console.WriteLine("Sorry your choice is incorrect");
                     Console.WriteLine("Please make another choice.");
@@ -58,18 +61,35 @@ namespace AnimalConsoleApp
                 {
                     isChoiceValid = true;
                     Welcome();
+                    
                 }
             }
+
             return getChoiceInt;
         }
-        //method animals menu
-        public void animalMenu()
-        {
-            
-        }
+
         
-        //action actions menu
+        private int GetAnimalMenu()
+        {
+            var animalMenu = 0;
+            animalMenu = GetMenuChoice("Please make a choice", MainMenu);
+            
+            while (animalMenu != EXIT_MENU_CHOICE)
+            {
+                foreach (var animalType in Enum.GetValues(typeof(AnimalTypes)))
+                {
+                    Console.WriteLine("make a choice");
+                    Console.WriteLine(animalType);
+                }
+            }
+            return animalMenu;
+        }
     }
 }
+
+//method- return animals menu
+//method- return actions menu
+    
+
 
 
