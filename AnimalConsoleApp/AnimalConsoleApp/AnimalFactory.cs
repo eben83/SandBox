fa53e30f343cc2f4ffbc460778eeb4b178fb64e6
Eben Burger <eben_burger@me.com>
2020-04-09 10:48:48 +0200

    animal factory updated

--- AnimalConsoleApp/AnimalConsoleApp/AnimalFactory.cs
+++ AnimalConsoleApp/AnimalConsoleApp/AnimalFactory.cs
@@ -3,10 +3,28 @@ using System.Diagnostics;
 
 namespace AnimalConsoleApp
 {
-    public class AnimalFactory
+    public class AnimalFactory: IAnimal
     {
-        public void Create(AnimalTypes animalTypes)
+        static void Create(AnimalTypes animalTypes)
         {
+        }
+
+        public AnimalTypes Type { get; set; }
+        public string Name { get; set; }
+        public string Identification { get; set; }
+        public void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Eat()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Sleep()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
