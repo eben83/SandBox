51d461f2bc8ac8d51173d2935f2d4181b581765c
Eben Burger <eben_burger@me.com>
2020-04-17 13:08:17 +0200

    Numbers app added

---
+++ NumberApp/NumberApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
