20b3cb94417bf114fe35fb664753091c284148ad
Eben Burger <eben_burger@me.com>
2020-04-17 13:22:08 +0200

    added Show all numbers method

--- NumberApp/NumberApp/UiEngine.cs
+++ NumberApp/NumberApp/UiEngine.cs
@@ -1,10 +1,19 @@
+using System;
+
 namespace NumberApp
 {
     public class UiEngine
     {
         public void Run()
         {
-            
+        }
+
+        public void ShowAllNumbers()
+        {
+            for (int i = 0; i <= 300; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
