bca8b4243dfb4dbc7104493c0a91e036d879d3b5
Eben Burger <eben_burger@me.com>
2020-04-17 13:21:16 +0200

    added in UiEngine to Program class

--- NumberApp/NumberApp/Program.cs
+++ NumberApp/NumberApp/Program.cs
@@ -4,9 +4,9 @@ namespace NumberApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new UiEngine().Run();
         }
     }
 }
