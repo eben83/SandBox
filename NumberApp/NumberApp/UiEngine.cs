ab19b55a72183b8cd94793255136889428ff542a
Eben Burger <eben_burger@me.com>
2020-04-17 19:34:31 +0200

    moved the factor of 3 and 5 to the top of the loop

--- NumberApp/NumberApp/UiEngine.cs
+++ NumberApp/NumberApp/UiEngine.cs
@@ -9,26 +9,24 @@ namespace NumberApp
             var three = 3;
             var five = 5;
 
-            for (int i = 0; i <= 300; i++)
+            for (int i = 1; i <= 300; i++)
             {
-                if (i % three == 0)
-                {
-                    Console.WriteLine("pluto");
-                    continue;
-                }
-
-                if (i % five == 0)
-                {
-                    Console.WriteLine("mars");
-                    continue;
-                }
-
                 if (i % three == 0 && i % five == 0)
                 {
                     Console.WriteLine("Eureka");
                     continue;
                 }
+                if (i % three == 0)
+                {
+                    Console.WriteLine("Pluto");
+                    continue;
+                }
 
+                if (i % five == 0)
+                {
+                    Console.WriteLine("Mars");
+                    continue;
+                }
                 Console.WriteLine(i);
             }
         }
