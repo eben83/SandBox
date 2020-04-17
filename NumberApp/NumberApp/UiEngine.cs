efc97327788f2354a981b588918567451520ebd3
Eben Burger <eben_burger@me.com>
2020-04-17 14:28:01 +0200

    added return int to methods

--- NumberApp/NumberApp/UiEngine.cs
+++ NumberApp/NumberApp/UiEngine.cs
@@ -13,36 +13,43 @@ namespace NumberApp
         {
             for (int i = 1; i <= 300; i++)
             {
-                Console.WriteLine(i);
-                MultiplesOfNumberThree(i);
-                MultipleOfNumbersFive(i);
                 MultiplesOfNumberThreeAndFive(i);
+                MultipleOfNumbersFive(i);
+                MultiplesOfNumberThree(i);
+                Console.WriteLine(i);
             }
 
         }
 
-        public void MultiplesOfNumberThree(int x)
+        public int MultiplesOfNumberThree(int x)
         {
+            
             if (x % 3 == 0)
             {
                 Console.WriteLine($"{x}.Pluto");
             }
+
+            return x;
         }
 
-        public void MultipleOfNumbersFive(int x)
+        public int MultipleOfNumbersFive(int x)
         {
             if (x % 5 == 0)
             {
                 Console.WriteLine($"{x}. Mars");
             }
+
+            return x;
         }
 
-        public void MultiplesOfNumberThreeAndFive(int x)
+        public int MultiplesOfNumberThreeAndFive(int x)
         {
             if (x % 3 == 0 && x % 5 == 0)
             {
                 Console.WriteLine($"{x}. Eureka");
             }
+
+            return x;
         }
         
     }
