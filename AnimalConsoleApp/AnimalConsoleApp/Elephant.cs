232003fd0ae1be11bc5c9896b75f7a446d72bcc8
Eben Burger <eben_burger@me.com>
2020-04-19 22:41:56 +0200

    removed Properties and sleep method on Elephant class

--- AnimalConsoleApp/AnimalConsoleApp/Elephant.cs
+++ AnimalConsoleApp/AnimalConsoleApp/Elephant.cs
@@ -8,55 +8,20 @@ namespace AnimalConsoleApp
         {
             Type = AnimalTypes.Elephant;
         }
-
-        // public void Command(AnimalActions animalAction)
-        // {
-        //     switch(animalAction)
-        //     {
-        //         case AnimalActions.Run:
-        //             Run();
-        //             break;
-        //         case AnimalActions.Eat:
-        //             Eat();
-        //             break;
-        //         case AnimalActions.Sleep:
-        //             Sleep();
-        //             break;
-        //         case AnimalActions.Fly:
-        //             Fly();
-        //             break;
-        //         case AnimalActions.Talk:
-        //             Talk();
-        //             break;
-        //         default:
-        //             throw new ArgumentOutOfRangeException(nameof(animalAction), "Use the enum directly"); 
-        //     }
-        // }
         
-        private void Run()
+        protected override void Run()
         {
             Console.WriteLine("Whoosh");
             Console.WriteLine($"Play time for a {Type}, {Type}'s love to run");
             Console.WriteLine("They are the happiest while running around");
         }
-        private void Eat()
+        protected override void Eat()
         {
             Console.WriteLine("Burp");
             Console.WriteLine($"Look at this {Type}, how fast?");
             Console.WriteLine("How fast do they eat?");
         }
-        private void Sleep()
-        {
-            Console.WriteLine("zzzZZZZzzzZZZ");
-            Console.WriteLine("You looking for a warm water bottle");
-            Console.WriteLine($"A {Type}, they will want to sleep on top of you all year reound");
-        }
-        private void Fly()
-        {
-            Console.WriteLine("Fly? Dogs don't Fly");
-            Console.WriteLine("But Greyhounds can make it look like they can fly");
-        }
-        private void Talk()
+        protected override void Talk()
         {
             Console.WriteLine("Hello");
             Console.WriteLine("Bark!!");    
