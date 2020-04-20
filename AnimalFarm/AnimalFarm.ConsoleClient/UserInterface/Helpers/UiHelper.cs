c7ca26d383a7e90b37e08589e06494fda93c5865
Eben Burger <eben_burger@me.com>
2020-04-20 21:44:54 +0200

    moved methods around in Ui Engine

--- AnimalFarm/AnimalFarm.ConsoleClient/UserInterface/Helpers/UiHelper.cs
+++ AnimalFarm/AnimalFarm.ConsoleClient/UserInterface/Helpers/UiHelper.cs
@@ -2,6 +2,9 @@ namespace AnimalFarm.ConsoleClient.UserInterface.Helpers
 {
     public class UiHelper
     {
-        
+        public void ShowMenuFromEnum<T>(string message, T genericEnum)
+        {
+            
+        }
     }
 }
