f93cf1a27d0567efebb15c3d1b969f20e599c1ec
Eben Burger <eben_burger@me.com>
2020-04-20 23:04:21 +0200

    moved show animal and add animal methods to Helper

--- AnimalFarm/AnimalFarm.ConsoleClient/UserInterface/UiEngine.cs
+++ AnimalFarm/AnimalFarm.ConsoleClient/UserInterface/UiEngine.cs
@@ -25,7 +25,7 @@ namespace AnimalFarm.ConsoleClient.UserInterface
                         break;
 
                     case 1:
-                        ShowAnimalMenuAndAddAnimal(animals);
+                        AnimalMenuHelper.AddAnimal(animals);
                         menuChoice = 0;
                         break;
 
@@ -48,30 +48,6 @@ namespace AnimalFarm.ConsoleClient.UserInterface
             MainMenuHelper.ShowMainMenu();
             return MainMenuHelper.GetValidMainMenuChoice();
         }
-        private void ShowAnimalMenu()
-        {
-            Console.WriteLine("The following animals are available:");
-            foreach (var animalType in Enum.GetValues(typeof(AnimalTypes)))
-                Console.WriteLine($"{(int) animalType}. {animalType}");
-            Console.WriteLine();   
-        }
-        static void ShowAnimalActionMenu(AnimalTypes? animalType)
-        {
-            if (!animalType.HasValue)
-            {
-                Console.WriteLine("Which action would you like your animals to perform");
-            }
-            else
-            {
-                Console.WriteLine($"Which action would you like your {animalType} to perform");
-            }
-
-            foreach (var animalAction in Enum.GetValues(typeof(AnimalActions)))
-                Console.WriteLine($"{(int) animalAction}. {animalAction}");
-            
-            Console.WriteLine();
-            
-        }
         private void ShowAnimalListSummary(List<Animal> listOfAnimals)
         {
             if (listOfAnimals.Count == 0)
@@ -90,117 +66,22 @@ namespace AnimalFarm.ConsoleClient.UserInterface
                 Console.Write($" {pet.Identification} |");
             }
         }
-        private void ShowAnimalMenuAndAddAnimal(List<Animal> addAnimalToList)
-        {
-            UiHelper.ShowWelcomeMessage();
-            ShowAnimalMenu();
-
-            AnimalTypes animalType =(AnimalTypes) GetValidAnimalMenuChoice();
-
-            Console.WriteLine($"What name would you like to give your new {animalType}");
-            string animalName = Console.ReadLine();
-
-            Console.WriteLine($"Brilliant, You have named your {animalType}, {animalName}- " +
-                              $"Congratulations on the new member to the family...");
-            Console.WriteLine("Press Enter to carry on.");
-            Console.ReadLine();
-
-            //this will create a new animal Cat and not just an Animal.Cat
-            var animal = AnimalFactory.CreateAnimal(animalType, animalName);
-            addAnimalToList.Add(animal);
-        }
-        private int GetValidAnimalMenuChoice()
-        {
-            var menuSelection = 0;
-            var isAnimalMenuChoiceValid = false;
-            while (!isAnimalMenuChoiceValid)
-            {
-                Console.WriteLine("Which animal would you like to add?");
-
-                if (!int.TryParse(Console.ReadLine(), out menuSelection))
-                {
-                    UiHelper.ShowErrorMessage();
-                    UiHelper.ShowWelcomeMessage();
-                    ShowAnimalMenu();                    
-                }
-                else
-                {
-                    isAnimalMenuChoiceValid = true;
-                }
-            }
-            
-            return menuSelection;
-        }
         private void InteractWithOneAnimal(List<Animal> animalInteractList)
         {
-            ShowWelcomeMessage();
-            ShowInteractMenu(animalInteractList);
-            var selectedAnimal = GetAnimalFromMenu(animalInteractList);
-            ShowAnimalActionMenu(selectedAnimal.Type);
-            AnimalActions animalAction = (AnimalActions) GetValidAnimalActionMenuChoice(selectedAnimal.Type);
+            UiHelper.ShowWelcomeMessage();
+            AnimalMenuHelper.ShowInteractMenu(animalInteractList);
+            var selectedAnimal = AnimalMenuHelper.GetAnimalFromMenu(animalInteractList);
+            AnimalMenuHelper.ShowAnimalActionMenu(selectedAnimal.Type);
+            AnimalActions animalAction = (AnimalActions) AnimalMenuHelper.GetValidAnimalActionMenuChoice(selectedAnimal.Type);
             selectedAnimal.Command(animalAction);
             Console.WriteLine("Press enter to carry on:");
             Console.ReadLine();
         }
-        private void ShowInteractMenu(List<Animal> animalsList)
-        {
-            Console.WriteLine();
-            Console.WriteLine("You are able to interact with the following animals:");
-            Console.WriteLine("(To add more, follow the Add Animal menu option one)");
-            Console.WriteLine();
-            for (int i = 0; i < animalsList.Count; i++)
-            {
-                Console.WriteLine($"{i}. {animalsList[i].Identification}");
-            }
-        }
-        private Animal GetAnimalFromMenu(List<Animal> actualAnimals)
-        {
-            var actualAnimalsMenuChoice = 0;
-            var isActualAnimalMenuChoiceValid = false;
-            while (!isActualAnimalMenuChoiceValid)
-            {
-                Console.WriteLine("Which of your animals would you like to interact with?");
-
-                if (!int.TryParse(Console.ReadLine(), out actualAnimalsMenuChoice))
-                {
-                    ShowErrorMessage();
-                    ShowWelcomeMessage();
-                    ShowInteractMenu(actualAnimals);
-                }
-                else
-                {
-                    isActualAnimalMenuChoiceValid = true;
-                }
-            }
-
-            return actualAnimals[actualAnimalsMenuChoice];
-        }
-        private int GetValidAnimalActionMenuChoice(AnimalTypes? animalType)
-        {
-            var animalActionOption = 0;
-            var isActualAnimalActionOption = false;
-            
-            while (! isActualAnimalActionOption)
-            {
-                if (!int.TryParse(Console.ReadLine(), out animalActionOption))
-                {
-                    ShowErrorMessage();
-                    ShowWelcomeMessage();
-                    ShowAnimalActionMenu(animalType);
-                }
-                else
-                {
-                    isActualAnimalActionOption = true;
-                }
-            }
-
-            return animalActionOption;
-        }
         private void InteractWithAllAnimals(List<Animal> animals)
         {
-            ShowWelcomeMessage();
-            ShowAnimalActionMenu(null);
-            AnimalActions animalActionSelected = (AnimalActions) GetValidAnimalActionMenuChoice(null);
+            UiHelper.ShowWelcomeMessage();
+            AnimalMenuHelper.ShowAnimalActionMenu(null);
+            AnimalActions animalActionSelected = (AnimalActions) AnimalMenuHelper.GetValidAnimalActionMenuChoice(null);
 
             foreach (var animal in animals)
                 animal.Command(animalActionSelected);
