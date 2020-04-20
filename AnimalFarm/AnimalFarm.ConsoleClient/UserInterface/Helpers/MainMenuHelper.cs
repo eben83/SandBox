581919588895215f711d0145bfad40f6d3a32c89
Eben Burger <eben_burger@me.com>
2020-04-20 22:28:52 +0200

    Removed show welcome message, show error message

--- AnimalFarm/AnimalFarm.ConsoleClient/UserInterface/Helpers/MainMenuHelper.cs
+++ AnimalFarm/AnimalFarm.ConsoleClient/UserInterface/Helpers/MainMenuHelper.cs
@@ -1,7 +1,35 @@
+using System;
+using AnimalFarm.Core.Application;
+
 namespace AnimalFarm.ConsoleClient.UserInterface.Helpers
 {
-    public class MainMenuHelper
+    public static class MainMenuHelper
     {
-        
+        public static void ShowMainMenu()
+        {
+            UiHelper.ShowMenuFromEnum<MainMenuChoices>(null);
+        }
+        public static int GetValidMainMenuChoice()
+        {
+            var menuSelection = 0;
+            
+            var isMainMenuChoiceValid = false;
+            while (!isMainMenuChoiceValid)
+            {
+                Console.WriteLine("Please, make a selection, to carry on.");
+
+                if (!int.TryParse(Console.ReadLine(), out menuSelection))
+                {
+                    UiHelper.ShowErrorMessage();
+                    UiHelper.ShowWelcomeMessage();
+                    ShowMainMenu();
+                }
+                else
+                {
+                    isMainMenuChoiceValid = true;
+                }
+            }
+            return menuSelection;
+        }
     }
 }
