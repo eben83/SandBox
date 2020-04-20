e622ea8311adf1ffea5134e41bf0b28abc256d34
Eben Burger <eben_burger@me.com>
2020-04-20 09:51:03 +0200

    commented out old Switch, updated Create Animal- method to implement New Animal Factory

--- AnimalConsoleApp/AnimalConsoleApp/UiEngine.cs
+++ AnimalConsoleApp/AnimalConsoleApp/UiEngine.cs
@@ -39,23 +39,25 @@ namespace AnimalConsoleApp
         }
         private Animal CreateAnimal(AnimalTypes animalType, string animalName)
         {
-            switch (animalType)
-            {
-                case AnimalTypes.Dog:
-                    return new Dog(animalName);
-                case AnimalTypes.Cat:
-                    return new Cat(animalName);
-                case AnimalTypes.Elephant:
-                    return new Elephant(animalName);
-                case AnimalTypes.Salmon:
-                    return new Salmon(animalName);                    
-                case AnimalTypes.Penguin:
-                    return new Penguin(animalName);
-                case AnimalTypes.Eagle:
-                    return new Eagle(animalName);                    
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(animalType), animalType, null);
-            }
+            var createdAnimal = new AnimalFactory();
+            // switch (animalType)
+            // {
+            //     case AnimalTypes.Dog:
+            //         return new Dog(animalName);
+            //     case AnimalTypes.Cat:
+            //         return new Cat(animalName);
+            //     case AnimalTypes.Elephant:
+            //         return new Elephant(animalName);
+            //     case AnimalTypes.Salmon:
+            //         return new Salmon(animalName);                    
+            //     case AnimalTypes.Penguin:
+            //         return new Penguin(animalName);
+            //     case AnimalTypes.Eagle:
+            //         return new Eagle(animalName);                    
+            //     default:
+            //         throw new ArgumentOutOfRangeException(nameof(animalType), animalType, null);
+            // }
+            return null;
         }
         private int ShowMainMenuAndGetMainMenuChoice(List<Animal> animalsList)
         {
