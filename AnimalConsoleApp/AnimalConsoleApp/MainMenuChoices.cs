42a8acbe9a28411d276c3d2207de6b5e1e91a4fa
Eben Burger <eben_burger@me.com>
2020-04-20 10:55:30 +0200

    added and updated Main menu Enum

---
+++ AnimalConsoleApp/AnimalConsoleApp/MainMenuChoices.cs
@@ -0,0 +1,10 @@
+namespace AnimalConsoleApp
+{
+    public enum MainMenuChoices
+    {
+        AddAnAnimal = 1,
+        InteractWithOneAnimal = 2,
+        InteractWithAllAnimals = 3,
+        Exit = 4,
+    }
+}
