bb7275a72174ca657fb37e7ad2018680856e5b13
Eben Burger <eben_burger@me.com>
2020-04-20 23:16:21 +0200

    added show welcome, show error methods to Ui Helper

--- AnimalFarm/AnimalFarm.ConsoleClient/UserInterface/Helpers/UiHelper.cs
+++ AnimalFarm/AnimalFarm.ConsoleClient/UserInterface/Helpers/UiHelper.cs
@@ -3,9 +3,9 @@ using AnimalFarm.Core.Application;
 
 namespace AnimalFarm.ConsoleClient.UserInterface.Helpers
 {
-    public class UiHelper
+    public static class UiHelper
     {
-        public void ShowMenuFromEnum<T>(string message) where T : Enum
+        public static void ShowMenuFromEnum<T>(string message) where T : Enum
         {
             //checks for null value
             Console.WriteLine(message ?? string.Empty);
@@ -13,7 +13,21 @@ namespace AnimalFarm.ConsoleClient.UserInterface.Helpers
             foreach (T menu in Enum.GetValues(typeof(T))) 
                 Console.WriteLine($"{Convert.ToInt32(menu)}. {menu.GetDescription()}");
             
+            Console.WriteLine();        
+        }
+        
+        public static void ShowWelcomeMessage()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome, This is my one page Semi Refined Animal App, I hope you will enjoy it");
             Console.WriteLine();
+        }
+        
+        public static void ShowErrorMessage()
+        {
+            Console.WriteLine("Sorry your choice is incorrect");
+            Console.WriteLine("Please, try make another choice.");
+            Console.ReadLine();
         }
     }
 }
