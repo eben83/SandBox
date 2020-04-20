97b62639f36b655722545cde9bae8357ef4e97f9
Eben Burger <eben_burger@me.com>
2020-04-20 22:12:37 +0200

    Generic method

--- AnimalFarm/AnimalFarm.ConsoleClient/UserInterface/Helpers/UiHelper.cs
+++ AnimalFarm/AnimalFarm.ConsoleClient/UserInterface/Helpers/UiHelper.cs
@@ -1,10 +1,19 @@
+using System;
+using AnimalFarm.Core.Application;
+
 namespace AnimalFarm.ConsoleClient.UserInterface.Helpers
 {
     public class UiHelper
     {
-        public void ShowMenuFromEnum<T>(string message, T genericEnum)
+        public void ShowMenuFromEnum<T>(string message) where T : Enum
         {
+            //checks for null value
+            Console.WriteLine(message ?? string.Empty);
             
+            foreach (T menu in Enum.GetValues(typeof(T))) 
+                Console.WriteLine($"{Convert.ToInt32(menu)}. {menu.GetDescription()}");
+            
+            Console.WriteLine();
         }
     }
 }
