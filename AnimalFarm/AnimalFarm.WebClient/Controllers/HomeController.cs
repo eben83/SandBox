a0b7ab655577bc3fa28cac172d5ed0634c08ce2e
Eben Burger <eben_burger@me.com>
2020-04-22 19:22:38 +0200

    updated home controller

--- AnimalFarm/AnimalFarm.WebClient/Controllers/HomeController.cs
+++ AnimalFarm/AnimalFarm.WebClient/Controllers/HomeController.cs
@@ -23,9 +23,23 @@ namespace AnimalFarm.WebClient.Controllers
             return View();
         }
 
-        
+       public IActionResult AddAnimal()
+       {
+           return View();
+       }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+       public IActionResult InteractWithOneAnimal()
+       {
+           return View();
+       }
+
+       public IActionResult InteractWithAllAnimals()
+       {
+           return View();
+       }
+
+
+       [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
