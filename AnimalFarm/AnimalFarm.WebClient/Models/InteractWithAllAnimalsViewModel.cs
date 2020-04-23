3f481097c790bc5f8e9501f7a187d9e10d9a9507
Eben Burger <eben_burger@me.com>
2020-04-24 01:00:45 +0200

    updated Property on all animals view model

--- AnimalFarm/AnimalFarm.WebClient/Models/InteractWithAllAnimalsViewModel.cs
+++ AnimalFarm/AnimalFarm.WebClient/Models/InteractWithAllAnimalsViewModel.cs
@@ -2,6 +2,6 @@ namespace AnimalFarm.WebClient.Models
 {
     public class InteractWithAllAnimalsViewModel
     {
-        public Int AnimalAction { get; set; }
+        public int AnimalActions { get; set; }
     }
 }
