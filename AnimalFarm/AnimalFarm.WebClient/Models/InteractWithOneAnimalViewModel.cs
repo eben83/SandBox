60b1fd96df3b78f936a9519f9136e28d36ee4402
Eben Burger <eben_burger@me.com>
2020-04-23 23:59:58 +0200

    added interact with one animal view model

---
+++ AnimalFarm/AnimalFarm.WebClient/Models/InteractWithOneAnimalViewModel.cs
@@ -0,0 +1,8 @@
+namespace AnimalFarm.WebClient.Models
+{
+    public class InteractWithOneAnimalViewModel
+    {
+        public int SelectedAnimal { get; set; }
+        public int AnimalAction { get; set; }
+    }
+}
