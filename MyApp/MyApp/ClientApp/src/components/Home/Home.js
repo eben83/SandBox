b130d584ad192d4d2fd7eb1e90435ecb049f4331
Eben Burger <eben_burger@me.com>
2020-05-07 00:03:06 +0200

    updated home js

--- MyApp/MyApp/ClientApp/src/components/Home/Home.js
+++ MyApp/MyApp/ClientApp/src/components/Home/Home.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import "./Home.css";
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
     return (
-      <section className="p-5 text-center">
-        <h1>Welcome to my Portfolio</h1>
-        <h1>I'm A junior Trainee Developer</h1>
+      <section className="text-center home-image text-white-50">
+          <h1>Welcome to my Portfolio</h1>
+          <h1>I'm A junior Trainee Developer</h1>
+          <a className="btn btn-primary btn-dark">Click me</a>
       </section>
     );
   }
