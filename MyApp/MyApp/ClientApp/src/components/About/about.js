b810d8d71e01521f08088afed41367d19571ecdf
Eben Burger <eben_burger@me.com>
2020-05-07 01:19:33 +0200

    inserted text for About page

---
+++ MyApp/MyApp/ClientApp/src/components/About/about.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import "./about.css";
+
+const About = (props) => {
+
+    return (
+        <>
+            <h1>About</h1>
+        </>
+    );
+}
+export default About;
