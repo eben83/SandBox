d5a93c776d6e6cfd4926050e922dca6cdac3bb4a
Eben Burger <eben_burger@me.com>
2020-05-07 00:02:16 +0200

    removed nav from landing page

--- MyApp/MyApp/ClientApp/src/components/Layout.js
+++ MyApp/MyApp/ClientApp/src/components/Layout.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import {Container, Nav} from 'reactstrap';
 import './navbar/nav-bar';
-import NavBar from "./navbar/nav-bar";
-import {Home} from "./Home/Home";
+import { Home } from "./Home/Home";
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -10,7 +9,6 @@ export class Layout extends Component {
   render () {
     return (
       <div>
-        <NavBar />
         <Home />
         {/*<Container>*/}
         {/*  {this.props.children}*/}
