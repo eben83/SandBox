16c8e11c5381d3cf92e64206945f138d5c20f02e
Eben Burger <eben_burger@me.com>
2020-05-07 15:36:42 +0200

    new navgation file

---
+++ MyApp/MyApp/ClientApp/src/components/navbar/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Nav, Navbar } from "react-bootstrap";
+
+
+export const Navigation = () => (
+    <Navbar expand="lg">
+        <Navbar.Brand href="/">Eben Burger</Navbar.Brand>
+        <Navbar.Collapse>
+            <Nav className="ml-auto">
+                <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/training">Training</Nav.Link></Nav.Item>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+    
+)
