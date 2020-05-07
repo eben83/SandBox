1b0f7d605f32949da1b1ab4d09ca3234f8e6975a
Eben Burger <eben_burger@me.com>
2020-05-07 10:16:29 +0200

    updated import statements, removed broken anchor tag

--- MyApp/MyApp/ClientApp/src/components/Home/Home.js
+++ MyApp/MyApp/ClientApp/src/components/Home/Home.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import "./Home.css";
+import About from "../About/about";
+import {NavLink} from "reactstrap";
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -11,7 +13,6 @@ export class Home extends Component {
       <section className="text-center home-image text-white-50">
           <h1>Welcome to my Portfolio</h1>
           <h1>I'm A junior Trainee Developer</h1>
-          <a className="btn btn-dark btn-lg" href="../About/about.js">Click Me</a>
       </section>
     );
   }
