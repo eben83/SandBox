c81dbd73e37811243e6271420dec3d325da26ea1
Eben Burger <eben_burger@me.com>
2020-05-07 14:53:48 +0200

    updated about js file

--- MyApp/MyApp/ClientApp/src/components/About/about.js
+++ MyApp/MyApp/ClientApp/src/components/About/about.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import "./about.css";
 
-const About = () => {
-
-    return (
-        <section className="about text-center">
-            <h1>About</h1>
-            <div className="d-flex flex-wrap pt-5 w-50">
-                    <div className="col">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing bibendum est ultricies integer quis auctor. Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Dignissim sodales ut eu sem integer. Dignissim cras tincidunt lobortis feugiat vivamus. Mauris ultrices eros in cursus. Ultrices in iaculis nunc sed augue lacus viverra vitae. Pulvinar etiam non quam lacus suspendisse faucibus interdum. Proin sagittis nisl rhoncus mattis rhoncus urna. Lobortis scelerisque fermentum dui faucibus in ornare.
-                            Magna fermentum iaculis eu non diam. Massa eget egestas purus viverra accumsan. Nunc congue nisi vitae suscipit. In metus vulputate eu scelerisque felis imperdiet. Mauris cursus mattis molestie a iaculis at. Donec et odio pellentesque diam volutpat commodo. Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis. Nisi vitae suscipit tellus mauris a. Faucibus turpis in eu mi bibendum neque egestas. Quis viverra nibh cras pulvinar mattis nunc sed. Amet massa vitae tortor condimentum lacinia quis vel. Mi bibendum neque egestas congue quisque egestas diam in. Ultrices mi tempus imperdiet nulla. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Vitae auctor eu augue ut lectus. Mi bibendum neque egestas congue quisque. Pharetra et ultrices neque ornare.</p>
-                    </div>
-                <div>
-                    <img src="/Images/shahadat-rahman-BfrQnKBulYQ-unsplash.jpg" alt="Computer Image"/>
-                </div>
+export const About = () => (
+    <section className="about text-center">
+        <h1>About</h1>
+        <div className="d-flex flex-wrap pt-5 w-50">
+            <div className="col">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing bibendum est ultricies integer quis auctor. Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Dignissim sodales ut eu sem integer. Dignissim cras tincidunt lobortis feugiat vivamus. Mauris ultrices eros in cursus. Ultrices in iaculis nunc sed augue lacus viverra vitae. Pulvinar etiam non quam lacus suspendisse faucibus interdum. Proin sagittis nisl rhoncus mattis rhoncus urna. Lobortis scelerisque fermentum dui faucibus in ornare.
+                    Magna fermentum iaculis eu non diam. Massa eget egestas purus viverra accumsan. Nunc congue nisi vitae suscipit. In metus vulputate eu scelerisque felis imperdiet. Mauris cursus mattis molestie a iaculis at. Donec et odio pellentesque diam volutpat commodo. Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis. Nisi vitae suscipit tellus mauris a. Faucibus turpis in eu mi bibendum neque egestas. Quis viverra nibh cras pulvinar mattis nunc sed. Amet massa vitae tortor condimentum lacinia quis vel. Mi bibendum neque egestas congue quisque egestas diam in. Ultrices mi tempus imperdiet nulla. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Vitae auctor eu augue ut lectus. Mi bibendum neque egestas congue quisque. Pharetra et ultrices neque ornare.</p>
             </div>
-        </section>
-    );
-}
-export default About;
+            <div>
+                <img src="/Images/shahadat-rahman-BfrQnKBulYQ-unsplash.jpg" alt="Computer Image"/>
+            </div>
+        </div>
+    </section>
+)
