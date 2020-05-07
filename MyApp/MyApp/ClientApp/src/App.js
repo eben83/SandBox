b65401f0274c0583cf38b16852b62fdfbd178392
Eben Burger <eben_burger@me.com>
2020-05-07 10:14:01 +0200

    Updated about page

--- MyApp/MyApp/ClientApp/src/App.js
+++ MyApp/MyApp/ClientApp/src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home/Home';
-import About from './components/About/about';
+import { About } from './components/About/about';
 
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
-import './custom.css'
+import './custom.css';
 
 export default class App extends Component {
   static displayName = App.name;
