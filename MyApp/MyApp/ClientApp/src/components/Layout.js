ae66a733260ebd1451f976e132b3208cbeb050b3
Eben Burger <eben_burger@me.com>
2020-05-07 10:14:59 +0200

    updated layout page added nav, about, training pg

--- MyApp/MyApp/ClientApp/src/components/Layout.js
+++ MyApp/MyApp/ClientApp/src/components/Layout.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import {Container, Nav} from 'reactstrap';
 import './navbar/nav-bar';
 import { Home } from "./Home/Home";
+import { NavBar } from "./navbar/nav-bar";
+import About from "./About/about";
+import Training from "./Training/training";
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -10,9 +13,12 @@ export class Layout extends Component {
     return (
       <div>
         <Home />
-        <Container>
-          {this.props.children}
-        </Container>
+        <NavBar />
+        <About />
+        <Training />
+        {/*<Container>*/}
+          {/*{this.props.children}*/}
+        {/*</Container>*/}
       </div>
     );
   }
