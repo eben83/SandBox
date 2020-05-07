8c0e0517dbd94ef678d0e8a0e20d76e192148276
Eben Burger <eben_burger@me.com>
2020-05-07 14:54:52 +0200

    added and updated training page

--- MyApp/MyApp/ClientApp/src/components/Training/training.js
+++ MyApp/MyApp/ClientApp/src/components/Training/training.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import "./training.css";
 
-const Training = () => {
-
-    return (
-        <section className="text-center">
-            <h1>Training</h1>
-            <p>This is going to be all my training.</p>
-        </section>
-    );
-}
-export default Training;
+export const Training = () => (
+    <section className="text-center">
+        <h1>Training</h1>
+        <p>This is going to be all my training.</p>
+    </section>
+)
