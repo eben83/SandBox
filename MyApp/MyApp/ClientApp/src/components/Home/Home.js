bfe681578b208c8357b45b109367340d2ef6ceb7
Eben Burger <eben_burger@me.com>
2020-05-07 14:54:01 +0200

    updated home js

--- MyApp/MyApp/ClientApp/src/components/Home/Home.js
+++ MyApp/MyApp/ClientApp/src/components/Home/Home.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
-import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import "./Home.css";
-import About from "../About/about";
-import {NavLink} from "reactstrap";
 
-export class Home extends Component {
-  static displayName = Home.name;
-
-  render () {
-    return (
-      <section className="text-center home-image text-white-50">
-          <h1>Welcome to my Portfolio</h1>
-          <h1>I'm A junior Trainee Developer</h1>
-      </section>
-    );
-  }
-}
+export const Home = () => (
+    <section className="text-center home-image text-white-50">
+        <h1>Welcome to my Portfolio</h1>
+        <h1>I'm A junior Trainee Developer</h1>
+    </section>
+)
