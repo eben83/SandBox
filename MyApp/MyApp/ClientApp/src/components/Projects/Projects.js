211381e0d83dc7df6b1c2f9e1206bf1ad53cbb7e
Eben Burger <eben_burger@me.com>
2020-05-09 00:57:08 +0200

    added projects file js

---
+++ MyApp/MyApp/ClientApp/src/components/Projects/Projects.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import './Projects.css';
+
+export const Projects = () => (
+    <section className="text-center">
+        <h1>Projects</h1>
+        <p>Projects I did will go here</p>
+    </section>
+)
