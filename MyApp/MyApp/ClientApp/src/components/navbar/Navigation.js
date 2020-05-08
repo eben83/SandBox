9906bc13cddd5f22e6dfc10e4c468e6480e8b893
Eben Burger <eben_burger@me.com>
2020-05-09 00:56:33 +0200

    updated updated nav link added projects

--- MyApp/MyApp/ClientApp/src/components/navbar/Navigation.js
+++ MyApp/MyApp/ClientApp/src/components/navbar/Navigation.js
@@ -10,6 +10,7 @@ export const Navigation = () => (
                 <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
                 <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
                 <Nav.Item><Nav.Link href="/training">Training</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/Projects">Projects</Nav.Link></Nav.Item>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
