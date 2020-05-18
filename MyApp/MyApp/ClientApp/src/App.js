ef5999c1e200c962fbc9606658eee50fecf1b98b
Eben Burger <eben_burger@me.com>
2020-05-18 11:15:37 +0200

    updated switch statement to div- updated contact import

--- MyApp/MyApp/ClientApp/src/App.js
+++ MyApp/MyApp/ClientApp/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import { Home } from '../src/components/Home/Home'
 import { About } from '../src/components/About/about'
 import { Training } from '../src/components/Training/training'
-import { Projects } from '../src/components/Projects/Projects'
+import Projects  from '../src/components/Projects/Projects'
+import ContactMe from '../src/components/ContactMe/contactaMe'
 import { Layout } from "./components/Layout";
 import {Navigation} from "./components/navbar/Navigation";
 
@@ -17,13 +18,14 @@ export default class App extends Component {
       <React.Fragment>
           <Layout>
               <Router>
-                <switch>
+                <div>
                     <Route exact path="/" component={Home}/>
                     <Navigation />
                     <Route path="/About" component={About}/>
                     <Route path="/Training" component={Training}/>
                     <Route path="/Projects" component={Projects}/>
-                </switch>
+                    <Route path="/contactMe" component={ContactMe}/>
+                </div>
               </Router>
           </Layout>
       </React.Fragment>
