8b42f5f917af860b008e3d559f2565c1f5c2488d
Eben Burger <eben_burger@me.com>
2020-05-20 10:11:58 +0200

    added and updated Animal Farm View controller

---
+++ AnimalFarm/AnimalFarm.WebClient/Models/AnimalFarmViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using AnimalFarm.Core.Domain;
+
+namespace AnimalFarm.WebClient.Models
+{
+    public class AnimalFarmViewModel
+    {
+        List<Animal> Animals = new List<Animal>();
+        
+    }
+}
