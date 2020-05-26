77a7ee0a3c92e2f86dc213b244d8769d2cc5b028
Eben Burger <eben_burger@me.com>
2020-05-26 15:40:48 +0200

    elephant update- hunger added

--- AnimalFarm/AnimalFarm.Core/Domain/Elephant.cs
+++ AnimalFarm/AnimalFarm.Core/Domain/Elephant.cs
@@ -14,18 +14,21 @@ namespace AnimalFarm.Core.Domain
             Console.WriteLine("Whoosh");
             Console.WriteLine($"You might think- because of their size, they are slow.");
             Console.WriteLine($"Guess again, {Type}'s are a lot quicker than you think.");
+            Hungry -= 7;
         }
         protected override void Eat()
         {
             Console.WriteLine("Burp");
             Console.WriteLine($"This time, an {Type} can eat, they will devour ");
-            Console.WriteLine("A whole tree by thenselves");
+            Console.WriteLine("A whole tree by themselves");
+            Hungry += 6;
         }
         protected override void Talk()
         {
             Console.WriteLine("Hello");
             Console.WriteLine("Trumpet!!");    
             Console.WriteLine("Trumpet!!");
+            Hungry -= 4;
         }
 
     }
