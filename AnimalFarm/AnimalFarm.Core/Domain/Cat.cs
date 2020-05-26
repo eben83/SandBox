cadb2f805de5440787f1e17a7438a1afde6c2d38
Eben Burger <eben_burger@me.com>
2020-05-26 15:35:13 +0200

    cat update hunger added

--- AnimalFarm/AnimalFarm.Core/Domain/Cat.cs
+++ AnimalFarm/AnimalFarm.Core/Domain/Cat.cs
@@ -14,18 +14,21 @@ namespace AnimalFarm.Core.Domain
             Console.WriteLine("Whoosh");
             Console.WriteLine($"Running, They will rather watch");
             Console.WriteLine("You Run around");
+            Hungry -= 3;
         }
         protected override void Eat()
         {
             Console.WriteLine("Burp");
             Console.WriteLine($"I think you never feed your own {Type}");
             Console.WriteLine($"You probably feed everyone else's {Type}'s ");
+            Hungry += 4;
         }
         protected override void Talk()
         {
             Console.WriteLine("Hello");
             Console.WriteLine("Meow!!");    
             Console.WriteLine("Meow!!");
+            Hungry -= 1;
         }
 
     }
