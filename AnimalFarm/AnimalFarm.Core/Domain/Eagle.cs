d2fa48617d5f2c65b8486add082c93b8c507ed34
Eben Burger <eben_burger@me.com>
2020-05-26 15:41:27 +0200

    eagle update- hunger added

--- AnimalFarm/AnimalFarm.Core/Domain/Eagle.cs
+++ AnimalFarm/AnimalFarm.Core/Domain/Eagle.cs
@@ -14,24 +14,28 @@ namespace AnimalFarm.Core.Domain
             Console.WriteLine("Whoosh");
             Console.WriteLine($"Why? why would you think this");
             Console.WriteLine($"{Type} they don't need to run..");
+            Hungry -= 2;
         }
         protected override void Eat()
         {
             Console.WriteLine("Burp");
             Console.WriteLine($"If you have a weak tummy");
             Console.WriteLine("You better look away");
+            Hungry += 4;
         }
         protected override void Fly()
         {
             Console.WriteLine("Is it a bird?");
             Console.WriteLine("Is it a Plane?");
             Console.WriteLine($"It's a {Type}");
+            Hungry -= 6;
         }
         protected override void Talk()
         {
             Console.WriteLine("Hello");
             Console.WriteLine("Scream!!");    
             Console.WriteLine("Scream");
+            Hungry -= 2;
         }
     }
 }
