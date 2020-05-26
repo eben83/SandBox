3da43da04082481e0b743b63c5eebe0fd0a1c26f
Eben Burger <eben_burger@me.com>
2020-05-26 16:12:13 +0200

    salmon updated- hunger added

--- AnimalFarm/AnimalFarm.Core/Domain/Salmon.cs
+++ AnimalFarm/AnimalFarm.Core/Domain/Salmon.cs
@@ -14,18 +14,21 @@ namespace AnimalFarm.Core.Domain
             Console.WriteLine("Whoosh");
             Console.WriteLine($"Well, this type of run, Is not what you expect");
             Console.WriteLine($"Once a year there is something called a {Type} run");
+            Hungry -= 7;
         }
         protected override void Eat()
         {
             Console.WriteLine("Burp");
             Console.WriteLine($"Do {Type} even eat");
             Console.WriteLine("This might be one of the seven wonders of the world");
+            Hungry += 5;
         }
         protected override void Talk()
         {
             Console.WriteLine("Hello");
             Console.WriteLine("Bubble!!");    
             Console.WriteLine("Bubble!!");
+            Hungry -= 1;
         }
 
     }
