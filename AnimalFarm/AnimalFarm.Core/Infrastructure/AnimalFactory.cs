bc955f95f06a4fb668a22ef92ee63f820b59ce2f
Eben Burger <eben_burger@me.com>
2020-05-27 16:02:26 +0200

    removed setting the hungry property in the animal factory

--- AnimalFarm/AnimalFarm.Core/Infrastructure/AnimalFactory.cs
+++ AnimalFarm/AnimalFarm.Core/Infrastructure/AnimalFactory.cs
@@ -10,22 +10,16 @@ namespace AnimalFarm.Core.Infrastructure
             switch (animalTypes)
             {
                 case AnimalTypes.Dog:
-                    Animal.Hungry = 10;
                     return new Dog(animalName);
                 case AnimalTypes.Cat:
-                    Animal.Hungry = 10;
                     return new Cat(animalName);
                 case AnimalTypes.Elephant:
-                    Animal.Hungry = 10;
                     return new Elephant(animalName);
                 case AnimalTypes.Salmon:
-                    Animal.Hungry = 10;
                     return new Salmon(animalName);
                 case AnimalTypes.Penguin:
-                    Animal.Hungry = 10;
                     return new Penguin(animalName);
                 case AnimalTypes.Eagle:
-                    Animal.Hungry = 10;
                     return new Eagle(animalName);
                 default:
                     throw new ArgumentOutOfRangeException(nameof(animalTypes), animalTypes, null);
