1a34a448b1a21fcac5c66d0fa080c0f0b71f8b4b
Eben Burger <eben_burger@me.com>
2020-05-27 11:30:28 +0200

    penguin update- removed hungry property

--- AnimalFarm/AnimalFarm.Core/Domain/Penguin.cs
+++ AnimalFarm/AnimalFarm.Core/Domain/Penguin.cs
@@ -4,7 +4,6 @@ namespace AnimalFarm.Core.Domain
 {
     public class Penguin : Animal
     {
-        public int Hungry { get; set; } = 10;
         public Penguin(string name) : base(name)
         {
             Type = AnimalTypes.Penguin;
