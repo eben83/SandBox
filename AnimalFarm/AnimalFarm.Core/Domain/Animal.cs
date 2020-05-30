8d526e4bb28f25699df2bde2e429d4226818ffa2
Eben Burger <eben_burger@me.com>
2020-05-30 14:19:13 +0200

    updated Hunger Level prop to field, updated GetHungerLevel method to CalculateHungerLevel

--- AnimalFarm/AnimalFarm.Core/Domain/Animal.cs
+++ AnimalFarm/AnimalFarm.Core/Domain/Animal.cs
@@ -4,43 +4,21 @@ namespace AnimalFarm.Core.Domain
 {
     public abstract class Animal
     {
+        private int _hungerLevel;
+        
         public AnimalTypes Type { get; set; }
         public string Name { get; set; }
-        public int HungerLevel { get; set; }
+        public AnimalHungerStatus HungerStatus { get; set; }
         public string Identification
         {
-            get { return $" {Type} - {Name} - {GetHungerLevel()}"; }
+            get { return $" {Type} - {Name} - {HungerStatus}"; }
         }
         public Animal(string name)
         {
             Name = name;
-            HungerLevel = 10;
+            _hungerLevel = 10;
         }
-        public string GetHungerLevel()
-        {
-            if (HungerLevel == 10)
-            {
-                return "Full- lets go play";
-            }
-            else if (HungerLevel >=8)
-            {
-                return "I'm fine- let's do that again";
-            }
-            else if (HungerLevel >= 6)
-            {
-                return "I could do with a snack";
-            }
-            else if (HungerLevel >= 3)
-            {
-                return "I need food please";
-            }
-            else if (HungerLevel <= 3)
-            {
-                return "I'm not playing anymore";
-            }
-            
-            return null;
-        }
+        
         public void Command(AnimalActions animalAction)
         {
             switch(animalAction)
@@ -64,23 +42,54 @@ namespace AnimalFarm.Core.Domain
                     throw new ArgumentOutOfRangeException(nameof(animalAction), "Use the enum directly"); 
             }
         }
-        public void Feed()
+        
+        protected void Feed()
         {
-            HungerLevel = 10;
+            _hungerLevel = 10;
         }
+
+        private void CalculateHungerStatus()
+        {
+            if (_hungerLevel == 10)
+            {
+                HungerStatus = AnimalHungerStatus.Full;
+                return;
+            }
+            if (_hungerLevel >=8)
+            {
+                HungerStatus = AnimalHungerStatus.Satisfied;
+                return;
+            }
+            if (_hungerLevel >= 6)
+            {
+                HungerStatus = AnimalHungerStatus.Snackish;
+                return;
+            }
+            if (_hungerLevel >= 3)
+            {
+                HungerStatus = AnimalHungerStatus.Hungry;
+                return;
+            }
+
+            HungerStatus = AnimalHungerStatus.Starving;
+        }
+        
         protected virtual void Run()
         {
-            Console.WriteLine("Animal is running");
+            _hungerLevel -= 5;
         }
+        
         //protected- access is limited
         protected virtual void Eat()
         {
-            Console.WriteLine("Animal is eating");
+            Feed();
         }
+        
         private void Sleep()
         {
-            Console.WriteLine("Animal is Sleeping");
+            _hungerLevel -= 2;
         }
+        
         protected virtual void Fly()
         {
             if (Type != AnimalTypes.Eagle)
@@ -89,10 +98,12 @@ namespace AnimalFarm.Core.Domain
                 return;
             }
             Console.WriteLine("Animal is Flying");
+            _hungerLevel -= 4;
         }
+        
         protected virtual void Talk()
         {
-            Console.WriteLine("Animal is Talking");
+            _hungerLevel -= 2;
         }
        
     }
