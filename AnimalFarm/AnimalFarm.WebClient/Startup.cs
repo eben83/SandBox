73dbccfc439e8d81e752547a16187f3370d94b2e
Dennis Arlow <dennis@synerics.com>
2020-05-30 08:20:38 +0200

    Register WebClient to use Newtonsoft.Json

--- AnimalFarm/AnimalFarm.WebClient/Startup.cs
+++ AnimalFarm/AnimalFarm.WebClient/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 
 namespace AnimalFarm.WebClient
 {
@@ -23,7 +24,7 @@ namespace AnimalFarm.WebClient
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
