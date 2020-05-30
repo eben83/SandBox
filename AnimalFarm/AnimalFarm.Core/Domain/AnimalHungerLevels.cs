869a19af2db079b0196faf66a842ce55dbf54a66
Eben Burger <eben_burger@me.com>
2020-05-30 11:32:42 +0200

    added new animal hunger level enum

---
+++ AnimalFarm/AnimalFarm.Core/Domain/AnimalHungerLevels.cs
@@ -0,0 +1,11 @@
+namespace AnimalFarm.Core.Domain
+{
+    public enum AnimalHungerLevels
+    {
+        Full = 10,
+        Satisfied = 8,
+        Snackish = 6,
+        Hungry = 4,
+        Starving = 0,
+    }
+}
