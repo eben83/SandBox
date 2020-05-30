a986ece5cae5eb7684c0cc4de45c489a6eb3a1a1
Eben Burger <eben_burger@me.com>
2020-05-30 14:21:56 +0200

    removed Hunger Level from Actions

--- AnimalFarm/AnimalFarm.Core/Domain/Eagle.cs
+++ AnimalFarm/AnimalFarm.Core/Domain/Eagle.cs
@@ -14,7 +14,6 @@ namespace AnimalFarm.Core.Domain
             Console.WriteLine("Whoosh");
             Console.WriteLine($"Why? why would you think this");
             Console.WriteLine($"{Type} they don't need to run..");
-            HungerLevel -= 2;
         }
         protected override void Eat()
         {
@@ -28,14 +27,12 @@ namespace AnimalFarm.Core.Domain
             Console.WriteLine("Is it a bird?");
             Console.WriteLine("Is it a Plane?");
             Console.WriteLine($"It's a {Type}");
-            HungerLevel -= 6;
         }
         protected override void Talk()
         {
             Console.WriteLine("Hello");
             Console.WriteLine("Scream!!");    
             Console.WriteLine("Scream");
-            HungerLevel -= 2;
         }
     }
 }
