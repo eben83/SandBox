4d4523a419a6118506765ec8168f2c608a7cc00b
Eben Burger <eben_burger@me.com>
2020-06-01 09:51:17 +0200

    added updated hunger status - eagle

--- AnimalFarm/AnimalFarm.Core/Domain/Eagle.cs
+++ AnimalFarm/AnimalFarm.Core/Domain/Eagle.cs
@@ -12,6 +12,7 @@ namespace AnimalFarm.Core.Domain
         
         protected override List<string> Run()
         {
+            UpdateHungerStatus(5);
             _actionFeedback.Clear();
             _actionFeedback.Add("Whoosh");
             _actionFeedback.Add($"Why? why would you think this");
@@ -20,6 +21,7 @@ namespace AnimalFarm.Core.Domain
         }
         protected override List<string> Eat()
         {
+            UpdateHungerStatus(0);
             _actionFeedback.Clear();
             _actionFeedback.Add("Burp");
             _actionFeedback.Add($"If you have a weak tummy");
@@ -29,6 +31,7 @@ namespace AnimalFarm.Core.Domain
         }
         protected override List<string> Fly()
         {
+            UpdateHungerStatus(4);
             _actionFeedback.Clear();
             _actionFeedback.Add("Is it a bird?");
             _actionFeedback.Add("Is it a Plane?");
@@ -37,6 +40,7 @@ namespace AnimalFarm.Core.Domain
         }
         protected override List<string> Talk()
         {
+            UpdateHungerStatus(2);
             _actionFeedback.Clear();
             _actionFeedback.Add("Hello");
             _actionFeedback.Add("Scream!!");    
