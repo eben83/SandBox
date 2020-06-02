06b84133b13efaff5f0d63625be4f25f9b9e5ba2
Eben Burger <eben_burger@me.com>
2020-06-02 12:15:26 +0200

    Exercise 04- Exception Handling.

---
+++ C# 8 & dotNet Core 3/Chapter 3/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter a number between 0 and 255:");
+            var firstNumber = ReadLine();
+        
+            WriteLine("Enter a number between 0 and 255:");
+            var secondNumber = ReadLine();
+
+            try
+            {
+                var numberOne = int.Parse(firstNumber);
+                var numberTwo = int.Parse(secondNumber);
+                var total = numberOne * numberTwo;
+        
+                WriteLine($"{total}");
+            }
+            catch (FormatException)
+            {
+                WriteLine("Please enter a number");
+                throw;
+            }
+        }
+    }
+}
