c08336ad75352d39a9eee39ada73248b257e8623
Eben Burger <eben_burger@me.com>
2020-06-03 11:48:41 +0200

    updates and added new project

---
+++ Csharp/Chapter5/PacktLibrary/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace Packt.Shared
+{
+    public class Person: object
+    {
+        //fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
+    }
+}
---
+++ Csharp/Chapter5/PeopleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Console;
+using Packt.Shared;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bob = new Person();
+            bob.Name = "Bob Smith";
+            bob.DateOfBirth = new DateTime(1965, 12, 22);
+            bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+            
+            //storing multiple values using an enum type
+            bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon |
+                             WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+
+            WriteLine(format:
+                "{0} was born on {1: dddd, d MMMM yyyy}",
+                arg0: bob.Name,
+                arg1: bob.DateOfBirth);
+            
+            WriteLine(format:
+                "{0}'s favorite wonder is {1}. It's interger value is {2}",
+                arg0: bob.Name,
+                arg1: bob.FavoriteAncientWonder,
+                arg2: (int)bob.FavoriteAncientWonder);
+            
+            WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+            
+            var alice = new Person
+            {
+                Name = "Alice Jones",
+                DateOfBirth = new DateTime(1998, 3,7)
+            };
+            
+            WriteLine(format:
+                "{0} was born on {1: dd MMM yy}",
+                arg0: alice.Name,
+                arg1: alice.DateOfBirth
+                );
+        }
+    }
+}
