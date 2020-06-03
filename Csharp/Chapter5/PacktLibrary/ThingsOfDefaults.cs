49016a9abd00079a9a420fc9ea5787acc457e574
Eben Burger <eben_burger@me.com>
2020-06-03 14:05:13 +0200

    updated the default values- in constructor

--- Csharp/Chapter5/PacktLibrary/ThingsOfDefaults.cs
+++ Csharp/Chapter5/PacktLibrary/ThingsOfDefaults.cs
@@ -1,7 +1,22 @@
+using System;
+using System.Collections.Generic;
+
 namespace Packt.Shared
 {
     public class ThingsOfDefaults
     {
-        
+        public int Population;
+        public DateTime When;
+        public string Name;
+        public List<Person> People;
+
+        public ThingsOfDefaults()
+        {
+            //setting the constructor to the default values.
+            Population = default;
+            When = default;
+            Name = default;
+            People = default;
+        }
     }
 }
