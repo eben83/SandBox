6421f756b86090d284b15e2314d6b693f3da75a6
Eben Burger <eben_burger@me.com>
2020-06-03 13:40:43 +0200

    updated static field bank interest rate, used interest rate on more multiple instance, used constructor.

--- Csharp/Chapter5/PeopleApp/Program.cs
+++ Csharp/Chapter5/PeopleApp/Program.cs
@@ -17,6 +17,11 @@ namespace PeopleApp
             //storing multiple values using an enum type
             bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon |
                              WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+            
+            BankAccount.InterestRate = 0.012M;
+            
+            
+            
 
             WriteLine(format:
                 "{0} was born on {1: dddd, d MMMM yyyy}",
@@ -39,17 +44,23 @@ namespace PeopleApp
             {
                 WriteLine($"{bob.Children[child].Name}");
             }
+            
+            //this is using the const
+            WriteLine($"{bob.Name} is a {Person.Species}");
+            
+            //using the read- only
+            WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
+            
+            
 
             //not working correctly
-            foreach (var child in bob.Children)
-            {
-                WriteLine($"{child.ToString()}");
-            }
+            // foreach (var child in bob.Children)
+            // {
+            //     WriteLine($"{child.ToString()}");
+            // }
+            //
             
-            
-            
-            
-            
+            // new short hand to initialize the object.
             var alice = new Person
             {
                 Name = "Alice Jones",
@@ -61,6 +72,37 @@ namespace PeopleApp
                 arg0: alice.Name,
                 arg1: alice.DateOfBirth
                 );
+            
+            
+            
+            
+            var jonesAccount = new BankAccount();
+            jonesAccount.AccountName = "Mrs. Jones";
+            jonesAccount.Balance = 2400;
+            
+            WriteLine(format:
+                "{0} earned {1:C} interest",
+                arg0: jonesAccount.AccountName,
+                arg1: jonesAccount.Balance * BankAccount.InterestRate);
+            
+            var gerrierAccount = new BankAccount
+            {
+                AccountName = "Ms. Gerrier",
+                Balance = 98
+            };
+            
+            WriteLine(format:
+                "{0} earned {1:C} interest",
+                arg0: gerrierAccount.AccountName,
+                arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+            
+            //constructors initialized
+            var blankPerson = new Person();
+            WriteLine(format:
+                "{0} of {1} was created at {2: hh: mm: ss} on a {2: dddd}",
+                arg0: blankPerson.Name,
+                arg1: blankPerson.HomePlanet,
+                arg2: blankPerson.Instantiated);
         }
     }
 }
