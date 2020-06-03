34ce4afd8c48df2bf032562920927f9e15682cc2
Eben Burger <eben_burger@me.com>
2020-06-03 13:38:22 +0200

    added constant, read-only fields and constructor

--- Csharp/Chapter5/PacktLibrary/Person.cs
+++ Csharp/Chapter5/PacktLibrary/Person.cs
@@ -6,11 +6,27 @@ namespace Packt.Shared
 {
     public class Person: object
     {
-        //fields
+        //fields- use the instance to access
         public string Name;
         public DateTime DateOfBirth;
         public WondersOfTheAncientWorld FavoriteAncientWonder;
         public WondersOfTheAncientWorld BucketList;
         public List<Person> Children = new List<Person>();
+        
+        //constant- a field that will never change- use the class to access
+        public const string Species = "Homo Sapien";
+        
+        //read-only fields- use the instance to access
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+        
+        //constructors- when a field need to be initialized at runtime
+        public Person()
+        {
+            //set default values for fields
+            //including read-only fields
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
     }
 }
