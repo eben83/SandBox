2d91879b7e8b452ea8b6320d46a8d184f9ca42d2
Eben Burger <eben_burger@me.com>
2020-06-04 15:10:46 +0200

    instance and static method examples

--- Csharp/Chapter6/PeopleApp/Program.cs
+++ Csharp/Chapter6/PeopleApp/Program.cs
@@ -9,7 +9,25 @@ namespace PeopleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var harry = new Person {Name = "Harry"};
+            var mary = new Person {Name = "Mary"};
+            var jill = new Person {Name = "Jill"};
+            
+            //call instance method
+            var baby1 = mary.ProcreateWith(harry);
+            var baby3 = jill.ProcreateWith(mary);
+            
+            //call static method
+            var baby2 = Person.Procreate(harry, jill);
+            
+            WriteLine($"{harry.Name} has {harry.Children.Count} children");
+            WriteLine($"{mary.Name} has {mary.Children.Count} children");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children");
+            
+            WriteLine(format:
+                "{0}'s first child is named \"{1}\".",
+                arg0: harry.Name,
+                arg1: harry.Children[0].Name);
         }
     }
 }
