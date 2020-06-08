010231ffa9e5cc1ca78bf43960c07c249bd78332
Eben Burger <eben_burger@me.com>
2020-06-08 11:12:20 +0200

    hiding members

--- Csharp/Chapter6/PacktLibrary/Employee.cs
+++ Csharp/Chapter6/PacktLibrary/Employee.cs
@@ -9,5 +9,14 @@ namespace Packt.Shared
         //extending the class from the inherited class
         public string EmployeeCode { get; set; }
         public DateTime HiteDate { get; set; }
+
+        public new void WriteToConsole()
+        {
+            WriteLine(format:
+                "{0} was born on {1: dd/ MM/ yy} and hired on {2: dd/ MM/ yy}",
+                arg0: Name,
+                arg1: DateOfBirth,
+                arg2: HiteDate);
+        }
     }
 }
