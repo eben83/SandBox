e49e0e67466f92eef985c6df08057358d9ef0872
Eben Burger <eben_burger@me.com>
2020-06-08 10:31:08 +0200

    extending classes

--- Csharp/Chapter6/PacktLibrary/Employee.cs
+++ Csharp/Chapter6/PacktLibrary/Employee.cs
@@ -1,8 +1,13 @@
+using System;
 using static System.Console;
 
 namespace Packt.Shared
 {
     public class Employee: Person
     {
+        
+        //extending the class from the inherited class
+        public string EmployeeCode { get; set; }
+        public DateTime HiteDate { get; set; }
     }
 }
