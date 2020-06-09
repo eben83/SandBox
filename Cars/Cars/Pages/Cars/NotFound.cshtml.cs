27e50161da6e8ae3530b350a96aa021ebaa979ef
Eben Burger <eben_burger@me.com>
2020-06-09 17:27:14 +0200

    not found page added

---
+++ Cars/Cars/Pages/Cars/NotFound.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cars.Pages.Cars
+{
+    public class NotFound : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
