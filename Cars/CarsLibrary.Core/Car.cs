5a384a0a437f6d1a8aa718daed9cd920325fda8b
Eben Burger <eben_burger@me.com>
2020-06-09 10:46:50 +0200

    added properties to car class

--- Cars/CarsLibrary.Core/Car.cs
+++ Cars/CarsLibrary.Core/Car.cs
@@ -2,6 +2,10 @@ namespace CarsLibrary.Core
 {
     public class Car
     {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public string Colour { get; set; }
         
     }
 }
