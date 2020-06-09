d59ff2311362a1bb2875108dc9e06b7aa2dec625
Eben Burger <eben_burger@me.com>
2020-06-09 11:48:39 +0200

    updated list model- fields and ctor

--- Cars/Cars/Pages/Cars/List.cshtml.cs
+++ Cars/Cars/Pages/Cars/List.cshtml.cs
@@ -1,9 +1,21 @@
+using CarsLibrary.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace Cars.Pages.Cars
 {
     public class List : PageModel
     {
+        //allows to access the app setting files
+        private readonly IConfiguration config;
+        private readonly ICarData _carData; //field
+
+        
+        //ctor
+        public List(ICarData _carData)
+        {
+            this._carData = _carData;
+        }
         
         //the onGet responds to the http get
         public void OnGet()
