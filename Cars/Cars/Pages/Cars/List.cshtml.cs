2def430f49732cfd65ac39cb965e94cf740861ec
Eben Burger <eben_burger@me.com>
2020-06-09 10:31:42 +0200

    razor page added for the list

---
+++ Cars/Cars/Pages/Cars/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cars.Pages.Cars
+{
+    public class List : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
