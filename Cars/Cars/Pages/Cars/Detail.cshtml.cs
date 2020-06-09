cff1ad4ec141db7002790a06810b6115ab24b7fb
Eben Burger <eben_burger@me.com>
2020-06-09 15:25:30 +0200

    added ctor and updated on get method to search by id

--- Cars/Cars/Pages/Cars/Detail.cshtml.cs
+++ Cars/Cars/Pages/Cars/Detail.cshtml.cs
@@ -1,16 +1,23 @@
 using CarsLibrary.Core;
+using CarsLibrary.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Cars.Pages.Cars
 {
     public class Detail : PageModel
     {
+        private ICarData carData;
         public Car Car { get; set; }
-        
+
+        //ctor
+        public Detail(ICarData carData)
+        {
+            this.carData = carData;
+        }
         public void OnGet(int carId)
         {
-            Car = new Car();
-            Car.Id = carId;
+            //saying that the car must equal the car id
+            Car = carData.GetById(carId);
         }
     }
 }
