d6d66ab539b6e5994e7b2bb7665264b68f0016e6
Eben Burger <eben_burger@me.com>
2020-06-09 18:10:00 +0200

    updated Edit razor

--- Cars/Cars/Pages/Cars/Edit.cshtml.cs
+++ Cars/Cars/Pages/Cars/Edit.cshtml.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using CarsLibrary.Core;
 using CarsLibrary.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Cars.Pages.Cars
 {
@@ -9,17 +11,21 @@ namespace Cars.Pages.Cars
     {
         //field
         private readonly ICarData _carData;
-        
+        private readonly IHtmlHelper _htmlHelper;
+
         //property
         public Car Car { get; set; }
+        public IEnumerable<SelectListItem> Cars { get; set; }
         
-        public Edit(ICarData carData)
+        public Edit(ICarData carData, IHtmlHelper htmlHelper)
         {
             this._carData = carData;
+            _htmlHelper = htmlHelper;
         }
         
         public IActionResult OnGet(int carId)
         {
+            Cars = _htmlHelper.GetEnumSelectList<CarType>();
             Car = _carData.GetById(carId);
             if (Car == null)
             {
