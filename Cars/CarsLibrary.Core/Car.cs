9d1ab1cdcc89625656c763845cde2622b1c7358b
Eben Burger <eben_burger@me.com>
2020-06-11 09:34:43 +0200

    added required fields

--- Cars/CarsLibrary.Core/Car.cs
+++ Cars/CarsLibrary.Core/Car.cs
@@ -7,8 +7,11 @@ namespace CarsLibrary.Core
         public int Id { get; set; }
         [Required]
         public string Model { get; set; }
+        [Required]
         public string Make { get; set; }
+        [Required]
         public string Colour { get; set; }
+        [Required]
         public CarType carType { get; set; }
     }
 }
