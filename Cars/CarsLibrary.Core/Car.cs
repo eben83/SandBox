c9fb4ad3ebd24ed8c19487063a45a3be481afc43
Eben Burger <eben_burger@me.com>
2020-06-11 08:46:15 +0200

    validation to the object class

--- Cars/CarsLibrary.Core/Car.cs
+++ Cars/CarsLibrary.Core/Car.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CarsLibrary.Core
 {
     public class Car
     {
         public int Id { get; set; }
+        [Required]
         public string Model { get; set; }
         public string Make { get; set; }
         public string Colour { get; set; }
