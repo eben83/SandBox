b4f14246ca04474a0fac1ec185608d8f697548cb
Eben Burger <eben_burger@me.com>
2020-06-11 14:16:35 +0200

    used ctor to pass in the connection string

--- Cars/CarsLibrary.Data/CarDbContext.cs
+++ Cars/CarsLibrary.Data/CarDbContext.cs
@@ -5,6 +5,13 @@ namespace CarsLibrary.Data
 {
     public class CarDbContext : DbContext
     {
+
+        public CarDbContext(DbContextOptions<CarDbContext> options)
+        :base(options)
+        {
+            
+        }
+        
         public DbSet<Car> Cars { get; set; }
     }
 }
