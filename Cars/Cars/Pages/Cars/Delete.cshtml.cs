696e8f10c87ef543dbd239fee6cedadb25a1b89d
Eben Burger <eben_burger@me.com>
2020-06-12 11:52:55 +0200

    delete page model added and ctor added

---
+++ Cars/Cars/Pages/Cars/Delete.cshtml.cs
@@ -0,0 +1,18 @@
+using CarsLibrary.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cars.Pages.Cars
+{
+    public class Delete : PageModel
+    {
+        public Delete(ICarData carData)
+        {
+            
+        }
+        
+        public void OnGet()
+        {
+            
+        }
+    }
+}
