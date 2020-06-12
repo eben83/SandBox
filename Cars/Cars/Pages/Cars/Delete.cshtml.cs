c394bb40ed7dc73520406b8dc7fcde0db8cb9b73
Eben Burger <eben_burger@me.com>
2020-06-12 12:16:40 +0200

    updated page model

--- Cars/Cars/Pages/Cars/Delete.cshtml.cs
+++ Cars/Cars/Pages/Cars/Delete.cshtml.cs
@@ -1,18 +1,44 @@
+using CarsLibrary.Core;
 using CarsLibrary.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Cars.Pages.Cars
 {
     public class Delete : PageModel
     {
+        private readonly ICarData _carData;
+
+        public Car Car { get; set; }
+        
         public Delete(ICarData carData)
         {
-            
+            _carData = carData;
         }
         
-        public void OnGet()
+        public IActionResult OnGet(int carId)
         {
-            
+            Car = _carData.GetById(carId);
+            if (Car == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+
+        public IActionResult onPost(int carId)
+        {
+            var car = _carData.Delete(carId);
+            _carData.Commit();
+
+            if (car == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            TempData["Message"] = $"{car.Make} deleted";
+            return RedirectToPage("./List");
         }
     }
 }
