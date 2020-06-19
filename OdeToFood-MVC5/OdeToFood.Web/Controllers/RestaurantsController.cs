16c7c9f913e92a40b7b126610b163372d38d9b7e
Eben <eben_burger@me.com>
2020-06-19 15:19:07 +0200

    updated index- actions (methods) added from restaurants index view

--- OdeToFood-MVC5/OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood-MVC5/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -1,13 +1,41 @@
 ﻿using System.Web.Mvc;
+using OdeToFood.Data.Services;
 
 namespace OdeToFood.Web.Controllers
 {
     public class RestaurantsController : Controller
     {
+        private readonly IRestaurantData _db;
+
+        public RestaurantsController(IRestaurantData db)
+        {
+            _db = db;
+        }
         // GET
         public ActionResult Index()
         {
-            return View();
+            var model = _db.GetAll();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionResult Details(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
