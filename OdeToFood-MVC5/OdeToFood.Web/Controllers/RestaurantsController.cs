302a77ecc16b2f4413d2426f11a708c245039e37
Eben <eben_burger@me.com>
2020-06-19 14:41:01 +0200

    added restaurants controller

---
+++ OdeToFood-MVC5/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
