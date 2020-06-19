2f973b2eb511b423d485d67a83896755325ccd3b
Eben <eben_burger@me.com>
2020-06-19 10:59:51 +0200

    added ref to OdeToFood.Data.Serveices, field for interface with ctor

--- OdeToFood-MVC5/OdeToFood.Web/Controllers/HomeController.cs
+++ OdeToFood-MVC5/OdeToFood.Web/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OdeToFood.Data.Services;
 
 namespace OdeToFood.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IRestaurantData db;
+
+        //ctor
+        public HomeController()
+        {
+            //new instance of in memory data
+            db = new InMemoryRestaurantData();
+        }
+        
         public ActionResult Index()
         {
-            return View();
+            var model = db.GetAll();
+            return View(model);
         }
 
         public ActionResult About()
