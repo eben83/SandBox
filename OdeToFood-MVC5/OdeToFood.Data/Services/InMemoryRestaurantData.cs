4db316563910eb026f5e4516647f2bda9dd71715
Eben <eben_burger@me.com>
2020-06-19 10:38:07 +0200

    added Dev data and updated interface

--- OdeToFood-MVC5/OdeToFood.Data/Services/InMemoryRestaurantData.cs
+++ OdeToFood-MVC5/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using OdeToFood.Data.Models;
 
 namespace OdeToFood.Data.Services
 {
     public class InMemoryRestaurantData : IRestaurantData
     {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizza", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Burgers", Cuisine = CuisineType.French},
+                new Restaurant {Id = 3, Name = "Woolies", Cuisine = CuisineType.None},
+            };
+        }
+        
+        
         public IEnumerable<Restaurant> GetAll()
         {
-            throw new System.NotImplementedException();
+            return restaurants.OrderBy(r => r.Name);
         }
     }
 }
