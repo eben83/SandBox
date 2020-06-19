8adb3b7feabcc43ea99f3e375615dc2466e7c87c
Eben <eben_burger@me.com>
2020-06-19 12:09:41 +0200

    added GreetingController

---
+++ OdeToFood-MVC5/OdeToFood.Web/Controllers/GreetingController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class GreetingController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
