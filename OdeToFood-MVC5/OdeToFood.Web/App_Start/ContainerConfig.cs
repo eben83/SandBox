f7aea8ea83d77c30d4ba4f4a222d6dc11ae2a312
Eben <eben_burger@me.com>
2020-06-19 13:08:51 +0200

    using the autoFac ContainerBuilder

--- OdeToFood-MVC5/OdeToFood.Web/App_Start/ContainerConfig.cs
+++ OdeToFood-MVC5/OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -1,10 +1,35 @@
-﻿namespace OdeToFood.Web
+﻿using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using OdeToFood.Data.Services;
+
+namespace OdeToFood.Web
 {
     public class ContainerConfig
     {
         public static void RegisterContainer()
         {
-            throw new System.NotImplementedException();
+            //builds the container
+            var builder = new ContainerBuilder();
+            
+            
+            //registering the controllers and the in memory data
+            //mvc app is the class from the behind global.asax class
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            
+            //used the in memory data
+            builder.RegisterType<InMemoryRestaurantData>()
+                .As<IRestaurantData>()
+                .SingleInstance();
+
+            
+            //builds the container to display the in memory data
+            var container = builder.Build();
+            
+            
+            //construction AutoFac container
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
