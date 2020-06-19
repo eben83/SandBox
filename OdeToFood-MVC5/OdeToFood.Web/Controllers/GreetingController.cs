d546b4386e31e02b94a2eb61584ea3e6183b42fd
Eben <eben_burger@me.com>
2020-06-19 12:39:50 +0200

    updated controller to access the GreetingViewModel to get message

--- OdeToFood-MVC5/OdeToFood.Web/Controllers/GreetingController.cs
+++ OdeToFood-MVC5/OdeToFood.Web/Controllers/GreetingController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Web.Mvc;
+using OdeToFood.Web.Models;
 
 namespace OdeToFood.Web.Controllers
 {
@@ -7,7 +9,9 @@ namespace OdeToFood.Web.Controllers
         // GET
         public ActionResult Index()
         {
-            return View();
+            var model = new GreetingViewModel();
+            model.Message = ConfigurationManager.AppSettings["message"];
+            return View(model);
         }
     }
 }
