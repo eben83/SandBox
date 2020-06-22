f6ff9475a8a62d63cb70b703ea25ce38332a7cec
Eben <eben_burger@me.com>
2020-06-22 10:48:55 +0200

    updated display name for Cuisine

--- OdeToFood-MVC5/OdeToFood.Data/Models/Restaurant.cs
+++ OdeToFood-MVC5/OdeToFood.Data/Models/Restaurant.cs
@@ -6,12 +6,12 @@ namespace OdeToFood.Data.Models
     public class Restaurant
     {
         public int Id { get; set; }
-        
-        
-        [Required]
+
+
+        [Required] 
         public string Name { get; set; }
         
-       
+        [Display(Name= "Type of Food")]
         public CuisineType Cuisine { get; set; }
     }
 }
