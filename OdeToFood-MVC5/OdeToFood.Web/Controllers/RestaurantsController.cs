27e779af628e86917aa6d6de12e98e35592964df
Eben <eben_burger@me.com>
2020-06-22 11:57:24 +0200

    added update method to controller

--- OdeToFood-MVC5/OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood-MVC5/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -57,7 +57,7 @@ namespace OdeToFood.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                
+                _db.Update(restaurant);
                 return RedirectToAction("Details", new {id = restaurant.Id});
             }
 
