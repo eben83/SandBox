36b8822dc9aaf656862038cf184da54985670f72
Eben <eben_burger@me.com>
2020-06-22 09:39:40 +0200

    example of ModelState validation

--- OdeToFood-MVC5/OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood-MVC5/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -29,7 +29,17 @@ namespace OdeToFood.Web.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Restaurant restaurant)
         {
-            _db.Add(restaurant);
+            if (string.IsNullOrEmpty(restaurant.Name))
+            {
+                ModelState.AddModelError(nameof(restaurant.Name), "The Name is required");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Add(restaurant);
+                return View();   
+            }
+
             return View();
         }
 
