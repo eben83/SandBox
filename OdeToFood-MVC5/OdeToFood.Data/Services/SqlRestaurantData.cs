2fceac6ddacc330d37a4a540aa0ebc29e556d298
Eben <eben_burger@me.com>
2020-06-22 13:04:48 +0200

    added ctor- added all interface methods- Update Add method

--- OdeToFood-MVC5/OdeToFood.Data/Services/SqlRestaurantData.cs
+++ OdeToFood-MVC5/OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -1,7 +1,36 @@
-﻿namespace OdeToFood.Data.Services
+﻿using System.Collections.Generic;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
 {
-    public class SqlRestaurantData
+    public class SqlRestaurantData :IRestaurantData
     {
+        private readonly OdeToFoodDbContext _db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            _db = db;
+        }
         
+        public IEnumerable<Restaurant> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Restaurant Get(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            _db.Restaurants.Add(restaurant);
+            _db.SaveChanges();
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
