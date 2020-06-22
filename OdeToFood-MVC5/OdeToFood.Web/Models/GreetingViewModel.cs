6f67ce79b6b456072c1bc530df23fa159daf56f9
Eben <eben_burger@me.com>
2020-06-22 08:04:46 +0200

    ViewModel- has everything to display. view used a model.

--- OdeToFood-MVC5/OdeToFood.Web/Models/GreetingViewModel.cs
+++ OdeToFood-MVC5/OdeToFood.Web/Models/GreetingViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace OdeToFood.Web.Models
+﻿using System;
+using System.Collections.Generic;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Web.Models
 {
     public class GreetingViewModel
     {
+        public IEnumerable<Restaurant> Restaurants { get; set; }
         public string Message { get; set; }
     }
 }
