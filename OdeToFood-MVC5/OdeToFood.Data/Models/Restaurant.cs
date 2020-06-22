25b6e9b8ea4d05a94d7476a14567883dfd3160c5
Eben <eben_burger@me.com>
2020-06-22 10:16:45 +0200

    added validation to the model

--- OdeToFood-MVC5/OdeToFood.Data/Models/Restaurant.cs
+++ OdeToFood-MVC5/OdeToFood.Data/Models/Restaurant.cs
@@ -1,9 +1,17 @@
-﻿namespace OdeToFood.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace OdeToFood.Data.Models
 {
     public class Restaurant
     {
         public int Id { get; set; }
+        
+        
+        [Required]
         public string Name { get; set; }
+        
+       
         public CuisineType Cuisine { get; set; }
     }
 }
