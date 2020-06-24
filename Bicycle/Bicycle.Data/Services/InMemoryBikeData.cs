9ab70c322cc0e84f4377c8b97b68d7f2f5522ca9
Eben <eben_burger@me.com>
2020-06-24 16:31:17 +0200

    interface implemented

--- Bicycle/Bicycle.Data/Services/InMemoryBikeData.cs
+++ Bicycle/Bicycle.Data/Services/InMemoryBikeData.cs
@@ -1,7 +1,14 @@
-﻿namespace Bicycle.Data.Services
+﻿using System.Collections.Generic;
+using Bicycle.Data.Models;
+
+namespace Bicycle.Data.Services
 {
     public class InMemoryBikeData : IBikeData
     {
-        
+        public IEnumerable<Bike> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
     }
+    
 }
