f65d443a60ea989e8baca7970667264a4b90a711
Eben <eben_burger@me.com>
2020-06-24 16:13:30 +0200

    removed class

--- Bicycle/Bicycle.Data/Models/Bike.cs
+++ Bicycle/Bicycle.Data/Models/Bike.cs
@@ -1,15 +1,15 @@
 ﻿namespace Bicycle.Data.Models
 {
-    public class Bicycle
+    public class Bike
     {
         public int Id { get; set; }
-        public BicycleType BicyleType { get; set; }
+        public string BicyleType { get; set; }
         public string BicycleMake { get; set; }
         public string BicycleModel { get; set; }
-        public GearSetMake GearSetMake { get; set; }
+        public string GearSetMake { get; set; }
         public string GearSet { get; set; }
-        public WheelSetType WheelSetType { get; set; }
-        public WheelSetMake WheelSetMake { get; set; }
+        public string WheelSetType { get; set; }
+        public string WheelSetMake { get; set; }
         public int Weight { get; set; }
     }
 }
