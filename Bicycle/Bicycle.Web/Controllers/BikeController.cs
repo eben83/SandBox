db0915e35b94431321715e2fd6ef9d20ebdf09f4
Eben <eben_burger@me.com>
2020-06-24 17:59:18 +0200

    Bike controller added

---
+++ Bicycle/Bicycle.Web/Controllers/BikeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Bicycle.Web.Controllers
+{
+    public class BikeController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
