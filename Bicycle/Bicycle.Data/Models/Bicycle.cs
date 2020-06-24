e65c631f8f34ea5e2e2a4a3fc47c0b1004d52cd6
Eben <eben_burger@me.com>
2020-06-24 15:53:22 +0200

    bicycle class updated

--- Bicycle/Bicycle.Data/Models/Bicycle.cs
+++ Bicycle/Bicycle.Data/Models/Bicycle.cs
@@ -3,12 +3,13 @@
     public class Bicycle
     {
         public int Id { get; set; }
-        public string BicyleType { get; set; }
+        public BicycleType BicyleType { get; set; }
         public string BicycleMake { get; set; }
-        public string GearSetMake { get; set; }
+        public string BicycleModel { get; set; }
+        public GearSetMake GearSetMake { get; set; }
         public string GearSet { get; set; }
-        public string WheelSetType { get; set; }
+        public WheelSetType WheelSetType { get; set; }
         public string WheelSetMake { get; set; }
-        public float Weight { get; set; }
+        public int Weight { get; set; }
     }
 }
