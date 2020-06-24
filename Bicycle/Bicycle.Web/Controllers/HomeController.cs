222ea5dff7cee935de4f49f7ce71c255a156275f
Eben <eben_burger@me.com>
2020-06-24 17:19:57 +0200

    added ctor and IBikeData field

--- Bicycle/Bicycle.Web/Controllers/HomeController.cs
+++ Bicycle/Bicycle.Web/Controllers/HomeController.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bike.Data.Services
 
 namespace Bicycle.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private IBikeDate db;
 
-        pubilc HomeController()
+        IBikeData db
+        
+        public HomeController()
         {
-            
+            db = new InMemoryBikeDate;
         }
-
+        
         public ActionResult Index()
         {
             return View();
