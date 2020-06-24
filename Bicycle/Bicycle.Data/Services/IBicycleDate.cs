b4a3dd671e907bc942582bf6bbd061cd4fadee95
Eben <eben_burger@me.com>
2020-06-24 14:23:56 +0200

    updated interface

--- Bicycle/Bicycle.Data/Services/IBicycleDate.cs
+++ Bicycle/Bicycle.Data/Services/IBicycleDate.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Generic;
+using Bicycle.Data.Models;
 
 namespace Bicycle.Data.Services
 {
     public interface IBicycleDate
     {
-        IEnumerable<Models.Bicycle> GetAll();
+        IEnumerable<BicycleType> GetAllBycycles();
+        IEnumerable<Models.Bicycle> GetAllBicycles();
+        IEnumerable<Models.WheelSet> GetAllWheelSets();
+        IEnumerable<Models.GearSet> GetAllGearSets();
+        
     }
 }
