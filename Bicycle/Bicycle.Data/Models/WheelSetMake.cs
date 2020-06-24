6b362f4d02b842dc70029621ece9761e3ce7bb96
Eben <eben_burger@me.com>
2020-06-24 16:24:25 +0200

    wheel set make class added updated

---
+++ Bicycle/Bicycle.Data/Models/WheelSetMake.cs
@@ -0,0 +1,9 @@
+﻿namespace Bicycle.Data.Models
+{
+    public class WheelSetMake
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Weight { get; set; }
+    }
+}
