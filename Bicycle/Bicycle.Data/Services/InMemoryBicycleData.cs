985808385943a646facae53c55e240dab790264a
Eben <eben_burger@me.com>
2020-06-24 14:24:26 +0200

    implemented missing members

--- Bicycle/Bicycle.Data/Services/InMemoryBicycleData.cs
+++ Bicycle/Bicycle.Data/Services/InMemoryBicycleData.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections.Generic;
+using Bicycle.Data.Models;
 
 namespace Bicycle.Data.Services
 {
     class InMemoryBicycleData : IBicycleDate
     {
-        public IEnumerable<Models.Bicycle> GetAll()
+        public IEnumerable<BicycleType> GetAllBycycles()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Models.Bicycle> GetAllBicycles()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<WheelSet> GetAllWheelSets()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<GearSet> GetAllGearSets()
         {
             throw new System.NotImplementedException();
         }
