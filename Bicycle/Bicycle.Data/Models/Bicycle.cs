f27624fd9a6e618f353fd3c62677768a22254ee0
Eben <eben_burger@me.com>
2020-06-24 15:45:59 +0200

    bicycle class update

--- Bicycle/Bicycle.Data/Models/Bicycle.cs
+++ Bicycle/Bicycle.Data/Models/Bicycle.cs
@@ -2,6 +2,13 @@
 {
     public class Bicycle
     {
-        
+        public int Id { get; set; }
+        public string BicyleType { get; set; }
+        public string BicycleMake { get; set; }
+        public string GearSetMake { get; set; }
+        public string GearSet { get; set; }
+        public string WheelSetType { get; set; }
+        public string WheelSetMake { get; set; }
+        public float Weight { get; set; }
     }
 }
