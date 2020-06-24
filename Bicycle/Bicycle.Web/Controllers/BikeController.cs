1e103fb927572bc1fd376f1d6b067acf583e81ad
Eben <eben_burger@me.com>
2020-06-24 18:53:36 +0200

    rename field

--- Bicycle/Bicycle.Web/Controllers/BikeController.cs
+++ Bicycle/Bicycle.Web/Controllers/BikeController.cs
@@ -1,13 +1,22 @@
 ﻿using System.Web.Mvc;
+using Bicycle.Data.Services;
 
 namespace Bicycle.Web.Controllers
 {
     public class BikeController : Controller
     {
+        IBikeData _db;
+
+        public BikeController()
+        {
+            _db = new InMemoryBikeData();
+        }
+        
         // GET
         public ActionResult Index()
         {
-            return View();
+            var model = _db.GetAll();
+            return View(model);
         }
     }
 }
