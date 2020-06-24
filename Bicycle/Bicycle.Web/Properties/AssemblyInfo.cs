995e7e0dc9c29b2ae0a7ce01b5ca23c8352daa3d
Eben <eben_burger@me.com>
2020-06-24 15:25:06 +0200

    refactor

--- Bicycle/Bicycle.Web/Properties/AssemblyInfo.cs
+++ Bicycle/Bicycle.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bicycle.Data")]
+[assembly: AssemblyTitle("Bicycle.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bicycle.Data")]
+[assembly: AssemblyProduct("Bicycle.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
