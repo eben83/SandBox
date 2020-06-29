467084f6c4f59335cfb3e744bcaebd3d1c310095
Eben <eben_burger@me.com>
2020-06-29 10:21:59 +0200

    updated display names WheelSetMake.cs

--- Bicycle/Bicycle.Data/Models/WheelSetMake.cs
+++ Bicycle/Bicycle.Data/Models/WheelSetMake.cs
@@ -1,9 +1,17 @@
-﻿namespace Bicycle.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bicycle.Data.Models
 {
     public class WheelSetMake
     {
+        
+        [Display(Name = "Wheel Set Name")]
         public string Name { get; set; }
+        
+        [Display(Name = "Wheel Set Model")]
         public string Model { get; set; }
+        
+        [Display(Name = "Wheel Set Weight")]
         public int Weight { get; set; }
     }
 }
