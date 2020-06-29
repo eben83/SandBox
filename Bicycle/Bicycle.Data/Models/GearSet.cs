58c301446316551e7ffacd827d6e70dd5212e3be
Eben <eben_burger@me.com>
2020-06-29 10:22:41 +0200

    updated display names GearSet.cs

--- Bicycle/Bicycle.Data/Models/GearSet.cs
+++ Bicycle/Bicycle.Data/Models/GearSet.cs
@@ -1,8 +1,13 @@
-﻿namespace Bicycle.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bicycle.Data.Models
 {
     public class GearSet
     {
+        [Display(Name = "Gear Set Model")]
         public string Model { get; set; }
+        
+        [Display(Name = "Gear Set Model Number")]
         public string ModelNumber { get; set; }
     }
 }
