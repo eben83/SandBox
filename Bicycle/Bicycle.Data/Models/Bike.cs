f428ba4fd066546fb6a4cc22be4c515989e33662
Eben <eben_burger@me.com>
2020-06-29 10:22:57 +0200

    updated display names Bike.cs

--- Bicycle/Bicycle.Data/Models/Bike.cs
+++ Bicycle/Bicycle.Data/Models/Bike.cs
@@ -1,15 +1,33 @@
-﻿namespace Bicycle.Data.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bicycle.Data.Models
 {
     public class Bike
     {
         public int Id { get; set; }
+        
+        [Display(Name ="Bike Type")]
         public BikeType BikeType { get; set; }
+        
+       [Display(Name = "Bike Make")]
         public string BikeMake { get; set; }
+        
+        [Display(Name = "Bike Model")]
         public string BikeModel { get; set; }
+        
         public GearSetMake GearSetMake { get; set; }
+        
+        [Display(Name = "Gear Set")]
         public GearSet GearSet { get; set; }
+        
+        [Display(Name = "Wheel Set Type")]
         public WheelSetType WheelSetType { get; set; }
+        
+        
         public WheelSetMake WheelSetMake { get; set; }
+        
+        [Display(Name = "Bike Weight")]
         public int Weight { get; set; }
     }
 }
