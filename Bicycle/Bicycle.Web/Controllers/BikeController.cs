98f92a6b9b1f1a3858a9d0bd7c84605deb3f6d2e
Eben <eben_burger@me.com>
2020-06-30 08:57:23 +0200

    added create method to handle post

--- Bicycle/Bicycle.Web/Controllers/BikeController.cs
+++ Bicycle/Bicycle.Web/Controllers/BikeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Bicycle.Data.Models;
 using Bicycle.Data.Services;
 
 namespace Bicycle.Web.Controllers
@@ -6,11 +8,12 @@ namespace Bicycle.Web.Controllers
     public class BikeController : Controller
     {
         private readonly IBikeData _db;
-        public BikeController()
+
+        public BikeController(IBikeData db)
         {
-            _db = new InMemoryBikeData();
+            _db = db;
         }
-        
+
         // GET
         public ActionResult Index()
         {
@@ -23,9 +26,28 @@ namespace Bicycle.Web.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Bike bike)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(bike);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
         public ActionResult Edit(int id)
         {
-            throw new System.NotImplementedException();
+            var model = _db.Get(id);
+            if (model == null)
+            {
+                RedirectToAction("NotFound");
+            }
+
+            return View(model);
         }
 
         public ActionResult Details(int id)
