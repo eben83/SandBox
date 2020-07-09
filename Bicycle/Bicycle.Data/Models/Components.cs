b1dcf3390a3d208683f5440aee3356fa9d07aea4
Eben <eben_burger@me.com>
2020-07-09 13:49:34 +0200

    updated component class display names

--- Bicycle/Bicycle.Data/Models/Components.cs
+++ Bicycle/Bicycle.Data/Models/Components.cs
@@ -1,9 +1,14 @@
-﻿namespace Bicycle.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bicycle.Data.Models
 {
     public class Components
     {
         public int Id { get; set; }
+        [Display(Name = "Bike Type")]
         public BikeType BikeType { get; set; }
+        
+        [Display(Name = "Component")]
         public ComponentType ComponentType { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
