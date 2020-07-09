59b70fb83b5ffd0a03b95c2043c96faf8c48efc6
Eben <eben_burger@me.com>
2020-07-09 09:57:04 +0200

    updated in memory to use Components.cs

--- Bicycle/Bicycle.Data/Services/InMemoryBikeData.cs
+++ Bicycle/Bicycle.Data/Services/InMemoryBikeData.cs
@@ -6,33 +6,32 @@ namespace Bicycle.Data.Services
 {
     public class InMemoryBikeData : IBikeData
     {
-        List<Bike> bikes;
+        List<Components> components;
 
         public InMemoryBikeData()
         {
-            bikes = new List<Bike>()
+            components = new List<Components>()
             {
-                new Bike{Id = 1, BikeType = BikeType.Road, BikeMake = "Merida", BikeModel = "Reacto 5000", GearSetMake = GearSetMake.Shimano, GearSet = new GearSet{Model = "Dura-Ace", ModelNumber = "8900"}, WheelSetType = WheelSetType.Aluminium, WheelSetMake = new WheelSetMake {Name = "Alex", Model = "Speed", Weight = 1200}, Weight = 8900},
-                new Bike{Id = 2, BikeType = BikeType.Road, BikeMake = "Merida", BikeModel = "Scultura 5000", GearSetMake = GearSetMake.Shimano, GearSet = new GearSet {Model = "Ultegra", ModelNumber = "6400"}, WheelSetType = WheelSetType.Aluminium, WheelSetMake = new WheelSetMake {Name = "Alex", Model = "Climber", Weight = 1200}, Weight = 7900},
-                new Bike{Id = 3, BikeType = BikeType.Road, BikeMake = "Trek", BikeModel = "Madone SLR6", GearSetMake = GearSetMake.Shimano, GearSet = new GearSet {Model = "Dura-Ace", ModelNumber = "8700"}, WheelSetType = WheelSetType.Carbon, WheelSetMake = new WheelSetMake {Name = "Bontrager", Model = "Aeolus Pro 5", Weight = 1200}, Weight = 6900},
-                new Bike{Id = 4, BikeType = BikeType.Road, BikeMake = "Trek", BikeModel = "Domane SL6", GearSetMake = GearSetMake.Shimano, GearSet = new GearSet {Model = "Ultegra", ModelNumber = "9700"}, WheelSetType = WheelSetType.Carbon, WheelSetMake = new WheelSetMake {Name = "Bontrager", Model = "Paradigm Comp 25", Weight = 1200}, Weight = 9200},
+                new Components{ComponentType = ComponentType.Pedals, Id = 1, Make = "Look", Model = "Carbon Blade", Weight = 321, BikeType = BikeType.Road},
+                new Components{ComponentType = ComponentType.Frame, Id = 2, Make = "Trek", Model = "Madona SLR", Weight = 1200, BikeType = BikeType.Road},
+                new Components{ComponentType = ComponentType.Cassette, Id = 3, Make = "Shimano", Model = "Ultegra", Weight = 110, BikeType = BikeType.Road},
             };
         }
         
-        public IEnumerable<Bike> GetAll()
+        public IEnumerable<Components> GetAll()
         {
-            return bikes.OrderBy(b => b.BikeMake);
+            return components.OrderBy(b => b.ComponentType);
         }
 
-        public Bike Get(int id)
+        public Components Get(int id)
         {
-            return bikes.FirstOrDefault(b => b.Id == id);
+            return components.FirstOrDefault(b => b.Id == id);
         }
 
-        public void Add(Bike bike)
+        public void Add(Components components)
         {
-            bikes.Add(bike);
-            bike.Id = bikes.Max(b => b.Id) + 1;
+            this.components.Add(components);
+            components.Id = this.components.Max(b => b.Id) + 1;
         }
     }
     
