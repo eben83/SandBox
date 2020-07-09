68ac0bdcfc26037c53bcfab853aa16e8a0d7a2a2
Eben <eben_burger@me.com>
2020-07-09 08:44:14 +0200

    component class added

---
+++ Bicycle/Bicycle.Data/Models/Components.cs
@@ -0,0 +1,9 @@
+﻿namespace Bicycle.Data.Models
+{
+    public class Components
+    {
+        public int Id { get; set; }
+        public BikeType Type { get; set; }
+        public string ComponentType { get; set; }
+    }
+}
