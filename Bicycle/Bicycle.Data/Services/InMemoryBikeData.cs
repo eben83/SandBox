8d69acf40b10deef696adff42242c9fef4757749
Eben <eben_burger@me.com>
2020-07-09 14:43:04 +0200

    updated in memory

--- Bicycle/Bicycle.Data/Services/InMemoryBikeData.cs
+++ Bicycle/Bicycle.Data/Services/InMemoryBikeData.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using Bicycle.Data.Models;
 
 namespace Bicycle.Data.Services
@@ -16,12 +17,13 @@ namespace Bicycle.Data.Services
                 new Components{ComponentType = ComponentType.Frame, Id = 2, Make = "Trek", Model = "Madona SLR", Weight = 1200, BikeType = BikeType.Road},
                 new Components{ComponentType = ComponentType.Cassette, Id = 3, Make = "Shimano", Model = "Ultegra", Weight = 110, BikeType = BikeType.Road},
                 new Components{ComponentType = ComponentType.Cassette, Id = 4, Make = "Shimano", Model = "Di-2", Weight = 150, BikeType = BikeType.Road},
+                new Components{ComponentType = ComponentType.Pedals, Id = 4, Make = "Look", Model = "Duo-Two", Weight = 500, BikeType = BikeType.Road},
             };
         }
         
         public IEnumerable<Components> GetAll()
         {
-            return components.OrderBy(b => b.Model);
+            return components;
         }
 
         public Components Get(int id)
