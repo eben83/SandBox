4811fc846e889986d0ae052c479c495fafd65eb6
Eben <eben_burger@me.com>
2020-07-09 08:35:18 +0200

    ContainerConfig.cs updated

--- Bicycle/Bicycle.Web/App_Start/ContainerConfig.cs
+++ Bicycle/Bicycle.Web/App_Start/ContainerConfig.cs
@@ -1,10 +1,24 @@
-﻿namespace Bicycle.Web
+﻿using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Bicycle.Data.Services;
+
+namespace Bicycle.Web
 {
     public class ContainerConfig
     {
         public static void RegisterContainer()
         {
-            throw new System.NotImplementedException();
+            var builder = new ContainerBuilder();
+
+            // mvc App is from the global.asax.cs file
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<InMemoryBikeData>()
+                .As<IBikeData>()
+                .SingleInstance();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
