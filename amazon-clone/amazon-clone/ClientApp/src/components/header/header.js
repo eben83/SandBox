a90c27ac856ba397d697b81f7fdf8791681a7cdf
Eben Burger <eben@arlowconsulting.co.za>
2020-10-17 10:22:05 +0200

    added logo and input search

---
+++ amazon-clone/amazon-clone/ClientApp/src/components/header/header.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+
+import './header.scss.sass'
+
+const Header = () => {
+    return (
+        <>
+            <div className='header'>
+                <img className='header__logo' 
+                     src='http://pngimg.com/uploads/amazon/amazon_PNG25.png' 
+                     alt='logo'/>
+                     
+                 <div className='header__search'>
+                     <input className='header__search-input' type='text' />
+                 </div>
+            </div>
+        </>
+    )
+}
+export default Header;
