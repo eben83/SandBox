b10b4d202a1e36e02ed9336567985fed71f21ca3
Eben Burger <eben@arlowconsulting.co.za>
2020-10-17 12:21:46 +0200

    home updated with banner and styling

---
+++ amazon-clone/amazon-clone/ClientApp/src/pages/home/home.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+
+import './home.css'
+
+const Home = () => {
+    return (
+        <>
+            <div className='home'>
+                <div className='home__container'>
+                    <img 
+                        className='home__image'
+                        src='https://techwiztime.com/wp-content/uploads/2016/08/amazon-prime-free-30-day-trial-banner-techwiztime.jpg'
+                        alt='amazon banner'
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+export default Home;
