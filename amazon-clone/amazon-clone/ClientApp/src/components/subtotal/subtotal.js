a2e47aa736f5aee175ffa1962f5d219d41905be5
Eben Burger <eben@arlowconsulting.co.za>
2020-10-18 07:28:31 +0200

    subtotal currency format component added

--- amazon-clone/amazon-clone/ClientApp/src/components/subtotal/subtotal.js
+++ amazon-clone/amazon-clone/ClientApp/src/components/subtotal/subtotal.js
@@ -1,10 +1,28 @@
 ﻿import React from 'react';
+import CurrencyFormat from 'react-currency-format';
+
+import './subtotal.css'
 
 const Subtotal = () => {
     return (
         <>
             <div className='subtotal'>
-                
+                <CurrencyFormat
+                renderText={(value) => (
+                    <>
+                        <p>
+                            Subtotal (0 items): <strong>0</strong>
+                        </p>
+                        <small className='subtotal__gift'>
+                            <input type='checkbox'/> This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                // value={getBasketTotal(basket)}
+                displayType={'text'}
+                prefix={'R'}
+                /> 
             </div>
         </>
     )
