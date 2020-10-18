45d882c5bc4164e4f858cf659f3455559e61e204
Eben Burger <eben@arlowconsulting.co.za>
2020-10-18 06:30:34 +0200

    cleaned up Layout.js

--- amazon-clone/amazon-clone/ClientApp/src/components/Layout.js
+++ amazon-clone/amazon-clone/ClientApp/src/components/Layout.js
@@ -1,18 +1,6 @@
 import React, { Component } from 'react';
 import Header from "./header/header";
 
-/*export class Layout extends Component {
-  static displayName = Layout.name;
-
-  render () {
-    return (
-      <div>
-        {this.props.children}
-      </div>
-    );
-  }
-}*/
-
 const Layout = (props) => {
   return (
       <>
