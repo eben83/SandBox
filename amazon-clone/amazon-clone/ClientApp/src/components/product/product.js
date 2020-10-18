aea1dd7d5e5a16a8270a5ce9a0d0093bf883f6ff
Eben Burger <eben@arlowconsulting.co.za>
2020-10-18 04:18:12 +0200

    product container added

--- amazon-clone/amazon-clone/ClientApp/src/components/product/product.js
+++ amazon-clone/amazon-clone/ClientApp/src/components/product/product.js
@@ -1,11 +1,27 @@
 ﻿import React from 'react'
+import StarRateIcon from '@material-ui/icons/StarRate';
 
 import './product.css'
 
 const Product = () => {
     return (
         <>
-            <div className='product'></div>
+            <div className='product'>
+                <div className='product__info'>
+                    <p>The Lean startup</p>
+                    <p className="product__price">
+                        <small>R</small>
+                        <strong>19,99</strong>
+                    </p>
+                    <div className='product__rating'>
+                        <p><StarRateIcon/></p>
+                    </div>
+                </div>
+                <img src='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg' 
+                     alt='the lean startup' 
+                />
+                <button> Add to basket</button>
+            </div>
         </>
     )
 }
