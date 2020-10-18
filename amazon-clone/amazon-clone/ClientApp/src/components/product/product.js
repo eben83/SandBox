64be35ff130494c01d1e121d7f4c3c60a5d0939b
Eben Burger <eben@arlowconsulting.co.za>
2020-10-18 03:39:20 +0200

    product component added

---
+++ amazon-clone/amazon-clone/ClientApp/src/components/product/product.js
@@ -0,0 +1,12 @@
+﻿import React from 'react'
+
+import './product.css'
+
+const Product = () => {
+    return (
+        <>
+            <div className='product'></div>
+        </>
+    )
+}
+export default Product;
