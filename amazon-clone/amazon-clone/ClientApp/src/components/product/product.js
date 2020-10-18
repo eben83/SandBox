1be98b10e970af55f24996ee4f0f4a50a9de2d0f
Eben Burger <eben@arlowconsulting.co.za>
2020-10-18 05:17:42 +0200

    added array for ratings

--- amazon-clone/amazon-clone/ClientApp/src/components/product/product.js
+++ amazon-clone/amazon-clone/ClientApp/src/components/product/product.js
@@ -3,22 +3,26 @@ import StarRateIcon from '@material-ui/icons/StarRate';
 
 import './product.css'
 
-const Product = () => {
+const Product = ({title, image, alt, price, rating}) => {
     return (
         <>
             <div className='product'>
                 <div className='product__info'>
-                    <p>The Lean startup</p>
+                    <p>{title}</p>
                     <p className="product__price">
                         <small>R</small>
-                        <strong>19,99</strong>
+                        <strong>{price}</strong>
                     </p>
                     <div className='product__rating'>
-                        <p><StarRateIcon/></p>
+                        {Array(rating)
+                            .fill()
+                            .map((_, i) => (
+                            <p><StarRateIcon /></p>
+                        ))}
                     </div>
                 </div>
-                <img src='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg' 
-                     alt='the lean startup' 
+                <img src={image} 
+                     alt={alt} 
                 />
                 <button> Add to basket</button>
             </div>
