bbe0d8848ee6959eea20ce97ed8ffd8610b2d605
Eben Burger <eben@arlowconsulting.co.za>
2020-10-19 08:11:01 +0200

    added state provider to index.js

--- amazon-clone/amazon-clone/ClientApp/src/index.js
+++ amazon-clone/amazon-clone/ClientApp/src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {StateProvider} from "./StateProvider";
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
-    <App />
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
   </BrowserRouter>,
   rootElement);
 
