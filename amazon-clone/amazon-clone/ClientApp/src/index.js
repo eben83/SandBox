98b3de917e9000569d7a6cd3604387a9af58a98c
Eben Burger <eben@arlowconsulting.co.za>
2020-10-19 09:10:46 +0200

    updated reducer to app.js

--- amazon-clone/amazon-clone/ClientApp/src/index.js
+++ amazon-clone/amazon-clone/ClientApp/src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {StateProvider} from "./StateProvider";
-import { initialState } from "./reducer";
+import StateProvider from "./StateProvider";
+import reducer, { initialState } from "./reducer";
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
